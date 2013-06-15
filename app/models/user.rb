b3e1de0d1f674b523e663ba600fec6edf719b5cc
Sven Fuchs <me@svenfuchs.com>
2013-06-15 23:51:40 +0200

    work on signup => profile, fix scaffolded links slim

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   ROLES = %w{ student coach mentor }.freeze
 
-  devise :registerable, :omniauthable
+  devise :omniauthable
 
   belongs_to :team
 
--- spec/views/users/show.html.slim_spec.rb
+++ spec/views/users/show.html.slim_spec.rb
@@ -4,7 +4,7 @@ describe 'users/show' do
   before(:each) do
     @user = assign(:user, stub_model(User,
       name: 'Name',
-      email: 'Email',
+      # email: 'Email',
       location: 'Location',
       bio: 'Bio',
       homepage: 'Homepage',
@@ -16,7 +16,7 @@ describe 'users/show' do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
-    rendered.should match(/Email/)
+    # rendered.should match(/Email/)
     rendered.should match(/Location/)
     rendered.should match(/Bio/)
     rendered.should match(/Homepage/)
