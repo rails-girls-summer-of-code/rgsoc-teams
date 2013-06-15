fb19d79e94ca76548f1dafabef362b4636b9f5fc
Trung Lê <joneslee85@gmail.com>
2013-06-16 02:08:16 +1000

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,22 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:member] do
     name     { Faker::Name.name }
     email    { Faker::Internet.email }
     location { Faker::Address.country }
     bio      { Faker::Lorem.paragraph }
     homepage { Faker::Internet.http_url }
     role     { User::ROLES.sample }
+
+    factory :coach do
+      role 'coach'
+    end
+
+    factory :student do
+      role 'student'
+    end
+
+    factory :mentor do
+      role 'mentor'
+    end
   end
 end
