ddc97631e0160f73d8fc0a7640e6c2519d77eb22
Trung Lê <joneslee85@gmail.com>
2013-06-17 09:00:20 +1000

    Add spec for User#github_url and #name_or_github_handle

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,23 @@ describe User do
   it { should have_many(:roles) }
   it { should validate_presence_of(:github_handle) }
   it { should validate_uniqueness_of(:github_handle) }
-  # it { should validate_uniqueness_of(:email) }
+
+  describe '#github_url' do
+    it 'should return github url' do
+      @user = User.new(github_handle: 'rails-girl')
+      @user.github_url.should == 'https://github.com/rails-girl'
+    end
+  end
+
+  describe '#name_or_handle' do
+    it 'should return name if existed' do
+      @user = User.new(name: 'trung')
+      @user.name_or_handle.should == 'trung'
+    end
+
+    it 'should return github_handle if name is not available' do
+      @user = User.new(github_handle: 'rails-girl')
+      @user.name_or_handle.should == 'rails-girl'
+    end
+  end
 end
