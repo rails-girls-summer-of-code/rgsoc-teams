bed5ed78b83f9e9b8fe1323a13eadd4f391c37d1
Sven Fuchs <me@svenfuchs.com>
2013-06-16 17:32:11 +0200

    order participants by name/github_handle, rename lists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.order('LOWER(COALESCE(name, github_handle))')
   end
 
   def show
