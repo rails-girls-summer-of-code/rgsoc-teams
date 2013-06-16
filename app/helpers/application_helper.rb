cf32f4316bb79948131c134f5b544db926e0ab25
Trung Lê <joneslee85@gmail.com>
2013-06-16 14:03:03 +1000

    Add spec for ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def link_to_team_members(team, role = :member)
     team.send(role.to_s.pluralize).map do |student|
-      link_to(h(student.name_or_handle), student)
+      link_to(student.name_or_handle, student)
     end.join(', ').html_safe
   end
 
-  def link_to_user_teams(user)
-    user.teams.map do |team|
-      link_to(h(team.name), team)
+  def link_to_user_roles(user)
+    user.roles.map do |role|
+      link_to("#{role.team.name} (#{role.name})", role.team)
     end.join(', ').html_safe
   end
 end
--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -3,5 +3,17 @@ FactoryGirl.define do
     team
     member
     name { Role::ROLES.sample }
+
+    factory :student_role do
+      name 'student'
+    end
+
+    factory :coach_role do
+      name 'coach'
+    end
+
+    factory :mentor_role do
+      name 'mentor'
+    end
   end
 end
