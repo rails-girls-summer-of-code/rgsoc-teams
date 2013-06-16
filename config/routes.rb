a7dda0ea50e82776c57dca23db6a7fcf19ec8b5f
Trung Lê <joneslee85@gmail.com>
2013-06-16 12:52:46 +1000

    devise_for no longer take block

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RgsocTeams::Application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' } do
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  devise_scope :user do
     delete 'sign_out' => 'devise/sessions#destroy'
   end
 
