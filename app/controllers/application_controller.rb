2e1ab7b2197b29aa53fdbca093bedb581e0ec3b6
Trung Lê <joneslee85@gmail.com>
2013-06-20 00:27:17 +1000

    Remove all references to users#new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, alert: exception.message
   end
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -7,10 +7,6 @@ describe UsersController do
       get('/users').should route_to('users#index')
     end
 
-    it 'routes to #new' do
-      get('/users/new').should route_to('users#new')
-    end
-
     it 'routes to #show' do
       get('/users/1').should route_to('users#show', id: '1')
     end
