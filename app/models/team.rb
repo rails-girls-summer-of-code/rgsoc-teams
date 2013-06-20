cf2da511de0f79a183a5cc6adfd5a75baf686606
Sven Fuchs <me@svenfuchs.com>
2013-06-20 12:57:21 +0200

    destroy dependent records

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,14 +3,14 @@ class Team < ActiveRecord::Base
   # validate :must_have_members
   # validate :must_have_unique_students
 
-  has_many :roles
+  has_many :roles, dependent: :destroy
   has_many :members,  class_name: 'User', through: :roles, source: :user
   has_many :students, class_name: 'User', through: :roles, source: :user, conditions: { roles: { name: 'student' } }
   has_many :coaches,  class_name: 'User', through: :roles, source: :user, conditions: { roles: { name: 'coach'   } }
   has_many :mentors,  class_name: 'User', through: :roles, source: :user, conditions: { roles: { name: 'mentor'  } }
 
-  has_many :repositories
-  has_many :activities
+  has_many :repositories, dependent: :destroy
+  has_many :activities, dependent: :destroy
 
   accepts_nested_attributes_for :roles, allow_destroy: true
 
