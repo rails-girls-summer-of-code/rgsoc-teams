74fbce1b0130c5da59cae9347f6b2ed5c5ae39e8
Sven Fuchs <me@svenfuchs.com>
2013-06-28 01:03:06 +0200

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ RgsocTeams::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'railsgirls.com',
+    user_name:            'summer-of-code-team@railsgirls.com',
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
