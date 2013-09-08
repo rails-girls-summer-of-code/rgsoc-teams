802d3df0b4e1d3fd860b170783d3143d5a0fcf5d
Andreas Tiefenthaler <at@an-ti.eu>
2013-09-08 22:07:29 +0200

    rename SECRET_TOKEN to RAILS_SESSION_SECRET

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-RgsocTeams::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '73d112362109022a68873f5942358fd50138e1edff8fafdd7ad668c93116f535ff035e58c959876c2b3c1b44c7a1ad708f5d7f2dfe936043920f5c2dd7535934'
+RgsocTeams::Application.config.secret_key_base = ENV['RAILS_SESSION_SECRET'] || '73d112362109022a68873f5942358fd50138e1edff8fafdd7ad668c93116f535ff035e58c959876c2b3c1b44c7a1ad708f5d7f2dfe936043920f5c2dd7535934'
