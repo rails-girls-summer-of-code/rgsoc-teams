f71b300f6ebd0c949fbb18701732c5b151029e20
Andreas Tiefenthaler <at@an-ti.eu>
2014-03-12 19:23:56 +0100

    adding rgsoc organizers mailing list

--- app/mailers/application_form_mailer.rb
+++ app/mailers/application_form_mailer.rb
@@ -1,11 +1,11 @@
 class ApplicationFormMailer < ActionMailer::Base
   default from: ENV['EMAIL_FROM']
 
-  default :to => "at@an-ti.eu"
+  default :to => 'summer-of-code@railsgirls.com'
 
   def new_application(application)
 
     @application = application
-    mail(to: 'at@an-ti.eu', subject: '[RGSoC 2014] New Application')
+    mail(subject: "[RGSoC 2014] New Application: #{@application.name}")
   end
 end
