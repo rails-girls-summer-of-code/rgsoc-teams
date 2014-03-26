176025beb35b498750cec3bc6ec541a21273bfd9
Carsten Zimmermann <cz@aegisnet.de>
2014-03-27 00:03:40 +0100

    Add country column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324172426) do
+ActiveRecord::Schema.define(version: 20140326225435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20140324172426) do
     t.string   "company_name"
     t.text     "company_info"
     t.boolean  "hide_email"
+    t.string   "country"
   end
 
 end
