c698227aee036a9f367cb0c2a0a8f284fcde08e5
Trung Lê <trung.le@ruby-journal.com>
2014-04-24 09:16:07 +1000

    Refactor routing specs

--- spec/routing/roles_routing_spec.rb
+++ spec/routing/roles_routing_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 
 describe RolesController do
   describe 'routing' do
-
     it 'routes to #new' do
-      get('/teams/1/roles/new').should route_to('roles#new', team_id: '1')
+      expect(get('/teams/1/roles/new')).to route_to('roles#new', team_id: '1')
     end
 
     it 'routes to #create' do
-      post('/teams/1/roles').should route_to('roles#create', team_id: '1')
+      expect(post('/teams/1/roles')).to route_to('roles#create', team_id: '1')
     end
 
     it 'routes to #destroy' do
-      delete('/teams/1/roles/1').should route_to('roles#destroy', team_id: '1', id: '1')
+      expect(delete('/teams/1/roles/1')).to route_to('roles#destroy', team_id: '1', id: '1')
     end
-
   end
 end
--- spec/routing/teams_routing_spec.rb
+++ spec/routing/teams_routing_spec.rb
@@ -2,34 +2,32 @@ require 'spec_helper'
 
 describe TeamsController do
   describe 'routing' do
-
     it 'routes to #index' do
-      get('/teams').should route_to('teams#index')
+      expect(get('/teams')).to route_to('teams#index')
     end
 
     it 'routes to #new' do
-      get('/teams/new').should route_to('teams#new')
+      expect(get('/teams/new')).to route_to('teams#new')
     end
 
     it 'routes to #show' do
-      get('/teams/1').should route_to('teams#show', id: '1')
+      expect(get('/teams/1')).to route_to('teams#show', id: '1')
     end
 
     it 'routes to #edit' do
-      get('/teams/1/edit').should route_to('teams#edit', id: '1')
+      expect(get('/teams/1/edit')).to route_to('teams#edit', id: '1')
     end
 
     it 'routes to #create' do
-      post('/teams').should route_to('teams#create')
+      expect(post('/teams')).to route_to('teams#create')
     end
 
     it 'routes to #update' do
-      put('/teams/1').should route_to('teams#update', id: '1')
+      expect(put('/teams/1')).to route_to('teams#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      delete('/teams/1').should route_to('teams#destroy', id: '1')
+      expect(delete('/teams/1')).to route_to('teams#destroy', id: '1')
     end
-
   end
 end
