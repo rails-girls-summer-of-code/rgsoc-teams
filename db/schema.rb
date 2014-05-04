3b3d71077aae217480991852f9fa6da6ff05ba06
Sven Fuchs <me@svenfuchs.com>
2014-05-04 15:07:23 +0200

    add coaching companies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504120403) do
+ActiveRecord::Schema.define(version: 20140504130203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140504120403) do
     t.text     "flags",                         default: [], array: true
     t.string   "country"
     t.string   "city"
+    t.string   "coaching_company"
   end
 
   create_table "attendances", force: true do |t|
