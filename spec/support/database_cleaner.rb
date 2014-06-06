45f6e74dfc1ba4cce0447f0a0adcf83f57c04253
Sebastian Gräßl <sebastian@validcode.me>
2014-06-06 10:06:08 +0200

    prefer befor over before(:each) in database_cleaner config

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -5,11 +5,11 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
+  config.before do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after do
     DatabaseCleaner.clean
   end
 
