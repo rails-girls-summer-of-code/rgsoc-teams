0ef8e6692924674e3e9b914e25d24515e9048918
Sebastian Gräßl <sebastian@validcode.me>
2014-06-06 10:41:07 +0200

    add specs for activity-scopes

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -5,15 +5,23 @@ describe Activity do
 
   describe 'scopes' do
     describe '.with_kind' do
-      pending 'TODO'
+      it 'queries for activities with a kind' do
+        kind = Activity::KINDS.sample
+        expect(Activity.with_kind(kind).where_values_hash).to eq("kind" => kind)
+      end
     end
 
     describe '.by_team' do
-      pending 'TODO'
+      it 'queries for activities with a team_id' do
+        team_id = rand(10)
+        expect(Activity.by_team(team_id).where_values_hash).to eq("team_id" => team_id)
+      end
     end
 
     describe '.ordered' do
-      pending 'TODO'
+      it 'queries with a specific order' do
+        expect(Activity.ordered.order_values).to eq(["published_at DESC, id DESC"])
+      end
     end
   end
 end
