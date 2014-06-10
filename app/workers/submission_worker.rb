119b3574301b604a50b526b8a11fe700f9a46702
Sebastian Gräßl <sebastian@validcode.me>
2014-06-10 10:37:52 +0200

    use logger instead of puts

--- app/workers/submission_worker.rb
+++ app/workers/submission_worker.rb
@@ -1,21 +1,25 @@
 class SubmissionWorker
   include SuckerPunch::Job
 
+  def logger
+    Rails.logger
+  end
+
   def perform(payload)
-    puts "dequeued submission: #{payload}"
+    logger.info "dequeued submission: #{payload}"
     ActiveRecord::Base.connection_pool.with_connection do
       submit(Submission.find(payload[:submission_id]))
     end
   end
 
   def submit(submission)
-    puts "about to deliver to: #{submission.to}"
+    logger.info "about to deliver to: #{submission.to}"
     submission.error = nil
     Mailer.email(submission).deliver
-    puts "done delivering to: #{submission.to}"
+    logger.info "done delivering to: #{submission.to}"
   rescue => e
-    puts "[error] delivering to: #{submission.to}"
-    puts e.message
+    logger.info "[error] delivering to: #{submission.to}"
+    logger.info e.message
     submission.error = e.message
   ensure
     submission.sent_at = Time.now
