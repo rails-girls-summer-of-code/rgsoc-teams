776371ceb18028729d378fa2e59b9bcab80caca2
Sebastian Gräßl <sebastian@validcode.me>
2014-07-02 23:47:19 +0200

    set EMAIL_FROM env var for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+ENV['EMAIL_FROM'] ||= 'test@rg.com'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
