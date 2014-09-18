e3c84075ca9e7b07a8ae7bd8322bf4704828ca0b
Carsten Zimmermann <cz@aegisnet.de>
2014-09-18 21:53:13 +0200

    Add job_offers table

---
+++ db/migrate/20140918195057_create_job_offers.rb
@@ -0,0 +1,21 @@
+class CreateJobOffers < ActiveRecord::Migration
+  def change
+    create_table :job_offers do |t|
+      t.string :title
+      t.text :description
+      t.string :url
+      t.string :company_name
+      t.string :contact_name
+      t.string :contact_email
+      t.string :contact_phone
+      t.string :city
+      t.string :country
+      t.string :duration
+      t.boolean :paid
+      t.boolean :public
+      t.text :misc_info
+
+      t.timestamps
+    end
+  end
+end
