4b9e19f8acb71fc55efd41e837a49601bc1df6a4
Carsten Zimmermann <cz@aegisnet.de>
2014-09-19 09:02:25 +0200

    Add job offer CRUD

--- app/controllers/job_offers_controller.rb
+++ app/controllers/job_offers_controller.rb
@@ -3,6 +3,21 @@ class JobOffersController < ApplicationController
 
   helper_method :job_offer, :job_offers
 
+  def create
+    job_offer.save!
+    redirect_to job_offer
+  end
+
+  def update
+    job_offer.update_attributes(job_offer_params)
+    redirect_to job_offer
+  end
+
+  def destroy
+    job_offer.destroy!
+    redirect_to job_offers_path
+  end
+
   private
 
   def job_offer
@@ -13,4 +28,18 @@ class JobOffersController < ApplicationController
     # TODO sorting
     @job_offers ||= JobOffer.all
   end
+
+  def job_offer_params
+    if params[:job_offer]
+      params.require(:job_offer).permit(
+        :company_name, :contact_email, :contact_name,
+        :contact_phone, :description, :duration,
+        :location, :misc_info, :paid,
+        :public, :title, :url
+      )
+    else
+      {}
+    end
+  end
+
 end
--- spec/controllers/job_offers_controller_spec.rb
+++ spec/controllers/job_offers_controller_spec.rb
@@ -12,6 +12,11 @@ describe JobOffersController do
     before { sign_in student }
   end
 
+  shared_context 'Admin logged in' do
+    let(:organizer) { create(:organizer) }
+    before { sign_in organizer }
+  end
+
   describe 'GET index' do
     context 'as an anonymous user' do
       it 'denies access' do
@@ -48,6 +53,123 @@ describe JobOffersController do
       it 'renders the show template' do
         get :show, { id: job_offer.to_param }, valid_session
         expect(response).to render_template 'show'
+      end
+    end
+  end
+
+  describe 'GET new' do
+    context 'as a student' do
+      include_context 'Student logged in'
+
+      it 'denies access' do
+        get :new, {}, valid_session
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as an admin' do
+      include_context 'Admin logged in'
+
+      it 'renders the new template' do
+        get :new, {}, valid_session
+        expect(response).to render_template 'new'
+      end
+    end
+  end
+
+  describe 'GET edit' do
+    context 'as a student' do
+      include_context 'Student logged in'
+
+      it 'denies access' do
+        get :edit, { id: job_offer.to_param }, valid_session
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as an admin' do
+      include_context 'Admin logged in'
+
+      it 'renders the edit template' do
+        get :edit, { id: job_offer.to_param }, valid_session
+        expect(response).to render_template 'edit'
+      end
+    end
+  end
+
+  describe 'POST create' do
+    let(:valid_attributes) { { job_offer: FactoryGirl.attributes_for(:job_offer) } }
+
+    context 'as a student' do
+      include_context 'Student logged in'
+
+      it 'denies access' do
+        expect {
+          post :create, valid_attributes, valid_session
+        }.not_to change { JobOffer.count }.by 1
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as an admin' do
+      include_context 'Admin logged in'
+
+      it 'creates record and redirects' do
+        expect {
+          post :create, valid_attributes, valid_session
+        }.to change { JobOffer.count }.by 1
+        expect(response).to redirect_to assigns(:job_offer)
+      end
+    end
+
+  end
+
+  describe 'PATCH update' do
+    context 'as a student' do
+      include_context 'Student logged in'
+
+      it 'denies access' do
+        patch :update, { id: job_offer.to_param, job_offer: { title: 'foo' } }, valid_session
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as an admin' do
+      include_context 'Admin logged in'
+
+      it 'updates record and redirects' do
+        new_title = SecureRandom.hex(10)
+        expect {
+          patch :update, { id: job_offer.to_param, job_offer: { title: new_title } }, valid_session
+        }.to change { job_offer.reload.title }.to new_title
+        expect(response).to redirect_to job_offer
+      end
+    end
+
+  end
+
+  describe 'DELETE destroy' do
+    before { job_offer }
+
+    context 'as a student' do
+      include_context 'Student logged in'
+
+      it 'denies access' do
+        expect {
+          delete :destroy, { id: job_offer.to_param }, valid_session
+        }.not_to change { JobOffer.count }.by(-1)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as an admin' do
+      include_context 'Admin logged in'
+
+      it 'destroys record and redirects' do
+        expect {
+          delete :destroy, { id: job_offer.to_param }, valid_session
+        }.to change { JobOffer.count }.by(-1)
+        expect(response).to redirect_to job_offers_path
       end
     end
   end
