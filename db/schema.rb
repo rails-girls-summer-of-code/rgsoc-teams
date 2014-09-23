f169dcaff79797cf6b79d0031bfe0570c02822fb
AkshataDM <oxta28@gmail.com>
2014-09-23 21:46:37 +0530

    Specs for team form

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140918195057) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
