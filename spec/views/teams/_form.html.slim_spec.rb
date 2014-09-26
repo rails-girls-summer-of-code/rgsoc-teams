7d392be014fd1ce14fb043c88e68b6c8ad195110
AkshataDM <oxta28@gmail.com>
2014-09-26 11:54:02 +0530

    Failing specs for view form

--- spec/views/teams/_form.html.slim_spec.rb
+++ spec/views/teams/_form.html.slim_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe 'teams/_form' do
-  let(:team) { stub_model(Team, name: 'XYZ', is_selected: false ) }
-  before(:each) do
-    assign(:team, team)
-    view.stub(:can?).with(:select, team).and_return(true)
+  before :each do
+    @team = Team.new
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+  end
+
+  describe 'admin must be able to select team'
+  before :each do
+    @ability.stub(:can?).with(:select, @team).and_return(true)
+  end
+  it 'should show the required field' do
+    render
+    expect(rendered).to match /Selected Team?/
   end
 end
