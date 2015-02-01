786f4cdc494fd18c2f48bdb695190075af50cd06
Carsten Zimmermann <cz@aegisnet.de>
2015-02-01 12:39:41 +0100

    Add Season model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024051654) do
+ActiveRecord::Schema.define(version: 20150201115659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,14 @@ ActiveRecord::Schema.define(version: 20141024051654) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "seasons", force: true do |t|
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sources", force: true do |t|
