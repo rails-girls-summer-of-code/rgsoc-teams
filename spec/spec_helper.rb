b1240df296244df63272d4f6f053f0257fa266a3
Carsten Zimmermann <cz@aegisnet.de>
2015-02-01 12:54:08 +0100

    WIP: infer spec type from location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,4 +47,6 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
   config.extend ControllerMacros,     type: :controller
+
+  config.infer_spec_type_from_file_location!
 end
