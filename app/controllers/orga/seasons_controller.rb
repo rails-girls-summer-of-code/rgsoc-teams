615256c37190d744b60808f3ae832c06186640a4
Carsten Zimmermann <cz@aegisnet.de>
2015-02-03 08:36:00 +0100

    Add season CRUD

--- app/controllers/orga/seasons_controller.rb
+++ app/controllers/orga/seasons_controller.rb
@@ -1,6 +1,51 @@
 class Orga::SeasonsController < Orga::BaseController
 
-  def index
-    render text: 'FIXME'
+  def new
+    starts_at = Time.utc(Date.today.year, 7, 1)
+    ends_at   = Time.utc(Date.today.year, 9, 30)
+    @season   = Season.new(starts_at: starts_at, ends_at: ends_at)
   end
+
+  def create
+    @season = Season.new(season_params)
+    if @season.save
+      redirect_to [:orga, @season], notice: "Season #{@season.name} created."
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @season = Season.find params[:id]
+  end
+
+  def index
+    @seasons = Season.all
+  end
+
+  def edit
+    @season = Season.find params[:id]
+  end
+
+  def update
+    @season = Season.find params[:id]
+    if @season.update season_params
+      redirect_to orga_seasons_path, notice: "Season #{@season.name} updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @season = Season.find params[:id]
+    @season.destroy
+    redirect_to orga_seasons_path, notice: "Season #{@season.name} has been deleted."
+  end
+
+  private
+
+  def season_params
+    params.require(:season).permit(:name, :starts_at, :ends_at)
+  end
+
 end
--- spec/controllers/orga/seasons_controller_spec.rb
+++ spec/controllers/orga/seasons_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 RSpec.describe Orga::SeasonsController do
+  render_views
+
   it_behaves_like 'redirects for non-admins'
+
+  context 'with admin logged in' do
+    include_context 'with admin logged in'
+
+    let(:season) { Season.create name: 2015 }
+
+    describe 'GET new' do
+      it 'renders the new template' do
+        get :new
+        expect(response).to be_success
+        expect(response).to render_template 'new'
+      end
+    end
+
+    describe 'GET index' do
+      it 'renders the index template' do
+        get :index
+        expect(response).to be_success
+        expect(response).to render_template 'index'
+      end
+    end
+
+    describe 'GET edit' do
+      it 'renders the edit template' do
+        get :edit, id: season.to_param
+        expect(response).to be_success
+        expect(response).to render_template 'edit'
+      end
+    end
+
+    describe 'GET show' do
+      it 'renders the show template' do
+        get :show, id: season.to_param
+        expect(response).to be_success
+        expect(response).to render_template 'show'
+      end
+    end
+  end
 end
