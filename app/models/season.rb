027e4dc0cadd9f939f9c64aaea1a660b10bbe2d1
Carsten Zimmermann <cz@aegisnet.de>
2015-02-22 20:11:26 +0100

    Validate Season#name

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,2 +1,3 @@
 class Season < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Season do
-  skip "add some examples to (or delete) #{__FILE__}"
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
 end
