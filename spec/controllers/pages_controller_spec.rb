3be7dc31a1b1675a3b0d507fde6f8ca05ff90d0e
Carsten Zimmermann <cz@aegisnet.de>
2015-03-02 08:40:25 +0100

    Add :page param

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe PagesController do
   render_views
 
   describe 'GET show' do
-    it 'renders the show template' do
-      get :show
-      expect(response).to render_template 'show'
+    it 'renders the help template' do
+      get :show, page: 'help'
+      expect(response).to render_template 'help'
     end
   end
-
 
 end
