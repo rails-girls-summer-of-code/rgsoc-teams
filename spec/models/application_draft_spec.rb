76f8d4883c35e2455e03628f7b639089e919fa9f
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-03-15 22:18:39 +0100

    Add 'Required Fields' array

--- spec/models/application_draft_spec.rb
+++ spec/models/application_draft_spec.rb
@@ -28,12 +28,7 @@ RSpec.describe ApplicationDraft do
     let(:student0) { Student.new }
     let(:student1) { Student.new }
 
-    [
-      :name, :application_about, :application_motivation, :application_gender_identification,
-      :application_coding_level, :application_community_engagement, :application_learning_period,
-      :application_learning_history, :application_skills,
-      :application_code_samples, :application_location, :banking_info, :application_minimum_money
-    ].each do |attribute|
+    Student::REQUIRED_DRAFT_FIELDS.each do |attribute|
 
       context 'with one student' do
         before { allow(subject).to receive(:students).and_return([student0]) }
