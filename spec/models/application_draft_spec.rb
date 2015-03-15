caf9cabd3545062d102361e5a994bad5e876d82f
Carsten Zimmermann <cz@aegisnet.de>
2015-03-15 21:47:28 +0100

    WIP: Add examples for studentX_<proxymethod>

--- spec/models/application_draft_spec.rb
+++ spec/models/application_draft_spec.rb
@@ -24,6 +24,59 @@ RSpec.describe ApplicationDraft do
     end
   end
 
+  context 'with proxy methods for its students' do
+    let(:student0) { Student.new }
+    let(:student1) { Student.new }
+
+    [
+      :name, :application_about, :application_motivation, :application_gender_identification,
+      :application_coding_level, :application_community_engagement, :application_learning_period,
+      :application_learning_history, :application_skills,
+      :application_code_samples, :application_location, :banking_info, :application_minimum_money
+    ].each do |attribute|
+
+      context 'with one student' do
+        before { allow(subject).to receive(:students).and_return([student0]) }
+
+        describe "#student0_#{attribute}" do
+          it "returns student##{attribute}" do
+            value = SecureRandom.hex(16)
+            expect(student0).to receive(attribute).and_return(value)
+            expect(subject.send("student0_#{attribute}")).to eql value
+          end
+        end
+
+        describe "#student1_#{attribute}" do
+          it "returns student##{attribute}" do
+            expect(subject.send("student1_#{attribute}")).to be_nil
+          end
+        end
+      end
+
+      context 'with two students' do
+        before { allow(subject).to receive(:students).and_return([student0, student1]) }
+
+        describe "#student0_#{attribute}" do
+          it "returns student##{attribute}" do
+            value = SecureRandom.hex(16)
+            expect(student0).to receive(attribute).and_return(value)
+            expect(subject.send("student0_#{attribute}")).to eql value
+          end
+        end
+
+        describe "#student1_#{attribute}" do
+          it "returns student##{attribute}" do
+            value = SecureRandom.hex(16)
+            expect(student1).to receive(attribute).and_return(value)
+            expect(subject.send("student1_#{attribute}")).to eql value
+          end
+        end
+      end
+
+    end
+
+  end
+
   describe '#role_for' do
     let(:user) { create :user }
     let(:team) { create :team }
