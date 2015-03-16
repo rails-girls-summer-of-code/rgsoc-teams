42087657474c302d0b559e9a0fea9c5b15b3d131
Carsten Zimmermann <cz@aegisnet.de>
2015-03-17 00:33:13 +0100

    Display draft's current state

--- app/helpers/application_drafts_helper.rb
+++ app/helpers/application_drafts_helper.rb
@@ -4,4 +4,9 @@ module ApplicationDraftsHelper
     current_student && current_student.id == student.try(:id)
   end
 
+  def draft_state(draft)
+    label_class = draft.state.draft? ? 'label-default' : 'label-success'
+    content_tag :span, draft.state.titleize, class: "label #{label_class}"
+  end
+
 end
