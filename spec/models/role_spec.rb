cd12dcdbecc5d8ffc39b882ddecfed8c51eb0ab3
Carsten Zimmermann <cz@aegisnet.de>
2015-03-19 23:35:46 +0100

    Remove deprecation for ensure_inclusion_of

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,7 +5,7 @@ describe Role do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to ensure_inclusion_of(:name).in_array(Role::ROLES) }
+  it { is_expected.to validate_inclusion_of(:name).in_array(Role::ROLES) }
   it { is_expected.to validate_uniqueness_of(:user_id).scoped_to([:team_id, :name]) }
 
   describe 'application' do
