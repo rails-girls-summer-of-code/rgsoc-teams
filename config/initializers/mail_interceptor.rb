b4d3b719cfa5f741f7957a3a3af7bfe59efac1a4
Carsten Zimmermann <cz@aegisnet.de>
2015-04-03 18:56:34 +0200

    Eagerly load mail* stuff when MAILTRAP_* vars are set

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,3 +1,5 @@
 if Rails.env.development? && [ENV['MAILTRAP_USER'], ENV['MAILTRAP_PASSWORD']].all?(&:present?)
+  require 'mail'
+  require 'lib/mailer_interceptor'
   Mail.register_interceptor(MailInterceptor)
 end
