05936dad2d550916fda2e9baca5bb3c8eea96fed
Carsten Zimmermann <cz@aegisnet.de>
2015-04-06 13:23:36 +0200

    Protect students' privacy for banking and money info

--- app/helpers/application_drafts_helper.rb
+++ app/helpers/application_drafts_helper.rb
@@ -6,6 +6,19 @@ module ApplicationDraftsHelper
       current_student.id == student.try(:id)
   end
 
+  def private_application_data(application_draft, &block)
+    block ||= Proc.new {}
+    header  = Proc.new do
+      content_tag :h4, "This section is hidden from your team's coaches and mentors"
+    end
+
+    if application_draft.as_student?
+      content_tag :div, class: 'bs-callout bs-callout-warning' do
+        capture(&header) + capture(&block)
+      end
+    end
+  end
+
   def draft_state(draft)
     label_class = draft.draft? ? 'label-default' : 'label-success'
     content_tag :span, draft.state.titleize, class: "label #{label_class}"
