e377fbac1040c09943bee2bd6f0a439f8e510f5c
Carsten Zimmermann <cz@aegisnet.de>
2015-04-07 22:19:48 +0200

    Remove requirement for misc_info

--- spec/models/application_draft_spec.rb
+++ spec/models/application_draft_spec.rb
@@ -53,7 +53,6 @@ RSpec.describe ApplicationDraft do
       it_behaves_like 'proxies :apply validation', :project_name
       it_behaves_like 'proxies :apply validation', :project_url
       it_behaves_like 'proxies :apply validation', :project_plan
-      it_behaves_like 'proxies :apply validation', :misc_info
       it_behaves_like 'proxies :apply validation', :heard_about_it
 
       context 'required fields for voluntary mode' do
