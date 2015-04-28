4b739a78069f9f876a6c41e25a334db802d96465
Marc Greenstock <marc@marcgreenstock.com>
2015-04-28 23:05:33 +0200

    added a with_applications trait to team factory

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,25 +2,32 @@ FactoryGirl.define do
   factory :team do
     kind 'sponsored'
     sequence(:name) { |i| "#{i}-#{FFaker::Lorem.word}" }
-  end
 
-  trait :helpdesk do
-    name 'helpdesk'
-  end
+    trait :helpdesk do
+      name 'helpdesk'
+    end
 
-  trait :supervise do
-    name 'supervise'
-  end
+    trait :supervise do
+      name 'supervise'
+    end
 
-  trait :applying_team do
-    name 'full team'
+    trait :applying_team do
+      name { FFaker::Product.brand }
 
-    after(:create) do |team|
-      %w(student student mentor coach).each do |role|
-        team.roles.create name: role, user: create(:user)
+      after(:create) do |team|
+        %w(student student mentor coach).each do |role|
+          team.roles.create name: role, user: create(:user)
+          team
+        end
       end
-      team
+    end
+
+    trait :with_applications do
+      after(:create) do |team|
+        # team.applications.create build(:application, team: nil).attributes
+        create_list :application, 2, team: team
+        team
+      end
     end
   end
-
 end
