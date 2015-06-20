66e05e502fbcc485f71586db8010d2615d74a8ca
Carsten Zimmermann <cz@aegisnet.de>
2015-06-20 09:34:58 +0200

    Use a shared example within a shared example. OMG!

--- spec/support/shared_examples/redirect_for_non_students.rb
+++ spec/support/shared_examples/redirect_for_non_students.rb
@@ -1,39 +1,33 @@
 RSpec.shared_examples 'redirects for non-users' do
   # OPTIMIZE test actions other than 'index'
 
+  shared_examples_for 'disallows access to students namespace' do
+    skip
+  end
+
   context 'as an admin' do
     skip
   end
 
   context 'as a guest' do
-    it 'redirects to / with an error message' do
-      skip
-    end
+    it_behaves_like 'disallows access to students namespace'
   end
 
   context 'as a coach' do
-    it 'redirects to / with an error message' do
-      skip
-    end
+    it_behaves_like 'disallows access to students namespace'
   end
 
   context 'as a mentor' do
-    it 'redirects to / with an error message' do
-      skip
-    end
+    it_behaves_like 'disallows access to students namespace'
   end
 
   context 'as a student' do
     context 'who is not poart of an accepted team' do
-      it 'redirects to / with an error message' do
-        skip
-      end
+      it_behaves_like 'disallows access to students namespace'
     end
 
     context 'who is part of an accepted team from last season' do
-      it 'redirects to / with an error message' do
-        skip
-      end
+      it_behaves_like 'disallows access to students namespace'
     end
   end
 
