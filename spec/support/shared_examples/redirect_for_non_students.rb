9f00339e86e9f2e46b5dba0161bf81d2df7b600f
Carsten Zimmermann <cz@aegisnet.de>
2015-06-20 09:32:57 +0200

    Transform spec stubs into contexts

--- spec/support/shared_examples/redirect_for_non_students.rb
+++ spec/support/shared_examples/redirect_for_non_students.rb
@@ -1,23 +1,40 @@
 RSpec.shared_examples 'redirects for non-users' do
+  # OPTIMIZE test actions other than 'index'
 
-  it 'allows an admin' do
+  context 'as an admin' do
     skip
   end
 
-  it 'redirects a coach' do
-    skip
+  context 'as a guest' do
+    it 'redirects to / with an error message' do
+      skip
+    end
   end
 
-  it 'redirects a mentor' do
-    skip
+  context 'as a coach' do
+    it 'redirects to / with an error message' do
+      skip
+    end
   end
 
-  it 'redirects a student who is not part of an accepted team' do
-    skip
+  context 'as a mentor' do
+    it 'redirects to / with an error message' do
+      skip
+    end
   end
 
-  it 'redirects a student who is part of an accepted team from last season' do
-    skip
+  context 'as a student' do
+    context 'who is not poart of an accepted team' do
+      it 'redirects to / with an error message' do
+        skip
+      end
+    end
+
+    context 'who is part of an accepted team from last season' do
+      it 'redirects to / with an error message' do
+        skip
+      end
+    end
   end
 
 end
