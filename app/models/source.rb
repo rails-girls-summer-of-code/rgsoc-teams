804f5609595ef1a130297723b8dc1f7daf709a7a
Carsten Zimmermann <cz@aegisnet.de>
2015-07-02 11:56:18 +0200

    Rename scope

--- app/models/source.rb
+++ app/models/source.rb
@@ -8,7 +8,7 @@ class Source < ActiveRecord::Base
   validates :url, presence: true
   validates :kind, presence: true
 
-  scope :accepted, ->() { joins(:team).where("teams.kind" => %w(sponsored voluntary)) }
+  scope :for_accepted_teams, ->() { joins(:team).where("teams.kind" => %w(sponsored voluntary)) }
 
   def url=(url)
     super(normalize_url(url))
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -4,7 +4,7 @@ describe Source do
   it { is_expected.to belong_to(:team) }
   it { is_expected.to validate_presence_of(:url) }
 
-  describe '.accepted' do
+  describe '.for_accepted_teams' do
     let!(:accepted_team)   { create :team, kind: %w(sponsored voluntary).sample }
     let!(:accepted_source) { create :source, team: accepted_team }
 
@@ -12,7 +12,7 @@ describe Source do
     let!(:unaccepted_source) { create :source, team: unaccepted_team }
 
     it 'only returns blogs of accepted teams' do
-      expect(described_class.accepted).to match_array [accepted_source]
+      expect(described_class.for_accepted_teams).to match_array [accepted_source]
     end
   end
 end
