869180fc928c770b8e180eeb31d3bfe0b2f54184
F3PiX <F3PiXnl@gmail.com>
2015-07-10 11:29:00 +0200

    FactoryGirl JobOffer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,50 @@
 # role = Role.create!([{name: 'organizer'}])
 # user = User.create!([{name: 'Wolverine', roles: role, github_handle: 'Adminis28'}])
 
-FactoryGirl.define do
-  factory :job_offer do
-    title         { FFaker::Lorem.sentence(5) }
-    description   { FFaker::Lorem.paragraph }
-    company_name  { FFaker::Company.name }
-    contact_email { FFaker::Internet.email }
-    location      { [ FFaker::Address.city, FFaker::Address.country ].join(', ') }
-  end
+#temp
+user = User.first
+if user
+user.roles.create(name: "organizer")
 end
+
+
+5.times do
+  cooljob = FactoryGirl.create(:job_offer_details)
+end
+
+# FactoryGirl.define do
+#   #Minimal definition with attributes that require validation
+#   factory :job_offer do
+#     title         { FFaker::Job.title }
+#     description   { FFaker::HipsterIpsum.paragraph }
+#     company_name  { FFaker::Company.name }
+#     contact_email { FFaker::Internet.email }
+#     location      { [ FFaker::Address.city, FFaker::Address.country ].join(', ') }
+#
+#     #Nested details inherit from :job_offer
+#     factory :job_offer_seeds_details do
+#       contact_name  { FFaker::Name.name }
+#       duration      { "#{[3,6,12,24].sample} months" }
+#       misc_info     { FFaker::Company.bs.capitalize }
+#       paid          { FFaker::Boolean.random }
+#     end
+#   end
+# end
+
+# contact_name  { FFaker::Name.name }
+# duration      { duration: "#{[3,6,12,24].sample} months" }
+# misc_info     { FFaker::Company.bs.capitalize }
+# paid          { FFaker::Boolean.random }
+#
+#
+# factory :post do
+#   title "A title"
+#
+#   factory :approved_post do
+#     approved true
+#   end
+# end
+#
+# approved_post = create(:approved_post)
+# approved_post.title    # => "A title"
+# approved_post.approved # => true
--- spec/factories/job_offers.rb
+++ spec/factories/job_offers.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
   factory :job_offer do
-    title         { FFaker::Lorem.sentence(5) }
-    description   { FFaker::Lorem.paragraph }
+    title         { FFaker::Job.title }
+    description   { FFaker::HipsterIpsum.paragraph }
     company_name  { FFaker::Company.name }
     contact_email { FFaker::Internet.email }
     location      { [ FFaker::Address.city, FFaker::Address.country ].join(', ') }
+
+    factory :job_offer_details do
+      contact_name  { FFaker::Name.name }
+      duration      { "#{[3,6,12,24].sample} months" }
+      misc_info     { FFaker::Company.bs.capitalize }
+      paid          { FFaker::Boolean.random }
+    end
   end
 end
