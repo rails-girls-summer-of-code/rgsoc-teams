f839783102064ddd2c542062b82d36443986ac66
F3PiX <F3PiXnl@gmail.com>
2015-07-11 13:31:33 +0200

    Seeding users with different roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
+FactoryGirl.create_list(:student, 7)
+FactoryGirl.create_list(:coach, 3)
+FactoryGirl.create :mentor
+FactoryGirl.create :supervisor
+FactoryGirl.create_list(:organizer, 2)
+
 FactoryGirl.create_list(:job_offer, 5, :with_details)
