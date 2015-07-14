aec31f266751ad1ac780f73d9b322fe67fba6bad
F3PiX <F3PiXnl@gmail.com>
2015-07-14 15:54:24 +0200

    UserSeeds in optima forma

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 FactoryGirl.create_list(:student, 12)
 FactoryGirl.create_list(:coach, 3)
 FactoryGirl.create_list(:organizer, 2)
-FactoryGirl.create (:mentor)
-FactoryGirl.create (:supervisor)
+FactoryGirl.create(:mentor)
+FactoryGirl.create(:supervisor)
 
 # To explore use cases where user has no role yet
 # NB These are not listed in the Community listing
