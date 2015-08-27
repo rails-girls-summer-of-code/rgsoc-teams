92d1f0d43cedb91303c87f214475092eea323331
F3PiX <F3PiXnl@gmail.com>
2015-08-27 23:28:49 +0200

    Cleaning up Dashb Controller

--- app/controllers/supervisor/dashboard_controller.rb
+++ app/controllers/supervisor/dashboard_controller.rb
@@ -1,9 +1,6 @@
 class Supervisor::DashboardController < Supervisor::BaseController
-  def index
-    #render text: "This is Visor.... SuperVisor"
-  end
 
-  def supervisee_teams
-    @supervisee_teams = current_user.teams
+  def index
   end
+  
 end
