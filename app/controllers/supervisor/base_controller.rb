c36af9561d070a678b0ba58e581275b2f202f76d
F3PiX <F3PiXnl@gmail.com>
2015-09-07 22:54:32 +0200

    Small improvements

--- app/controllers/supervisor/base_controller.rb
+++ app/controllers/supervisor/base_controller.rb
@@ -7,7 +7,7 @@ class Supervisor::BaseController < ApplicationController
 
   def must_be_supervisor
     unless signed_in? && current_user.roles.includes?('supervisor')
-      redirect_to root_path, alert: 'Sorry, dashboard is for supervisors only'
+      redirect_to root_path, alert: 'Sorry, the dashboard is for supervisors only'
     end
   end
 
