ac2d9aaafef8cb66f9bcabe5c3d145889e4e6097
F3PiX <F3PiXnl@gmail.com>
2015-09-10 13:06:19 +0200

    Restricts mailer to checks with text

--- app/controllers/supervisor/base_controller.rb
+++ app/controllers/supervisor/base_controller.rb
@@ -6,6 +6,7 @@ class Supervisor::BaseController < ApplicationController
   protected
 
   def must_be_supervisor
+    #TODO unless can? :supervise, @team
     unless signed_in? && current_user.roles.includes?('supervisor')
       redirect_to root_path, alert: 'Sorry, the dashboard is for supervisors only'
     end
