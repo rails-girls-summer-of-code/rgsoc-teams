a89dc40f004edea99628156562e9f48e9b953616
F3PiX <F3PiXnl@gmail.com>
2015-09-12 11:16:08 +0200

    Declutter

--- app/controllers/supervisor/base_controller.rb
+++ app/controllers/supervisor/base_controller.rb
@@ -6,7 +6,6 @@ class Supervisor::BaseController < ApplicationController
   protected
 
   def must_be_supervisor
-    #TODO unless can? :supervise, @team
     unless signed_in? && current_user.roles.includes?('supervisor')
       redirect_to root_path, alert: 'Sorry, the dashboard is for supervisors only'
     end
