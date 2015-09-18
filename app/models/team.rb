204d656b66bca5977f44bf6d508d1441df3bf0c7
F3PiX <F3PiXnl@gmail.com>
2015-09-18 14:28:46 +0200

    Extracts comments-score and updates-score from :performance method; better handling of scores when season is closed

--- app/models/team.rb
+++ app/models/team.rb
@@ -54,28 +54,59 @@ class Team < ActiveRecord::Base
 
   # Memo: Team Performance to show Performance Score in Supervisor's Dashboard
   def performance
-    return unless comments.any?
     @score = 0
-    latest_comment = comments.ordered.first
-    if latest_comment.created_at < Time.now-5.minute
-      @score = 2
-    elsif latest_comment.created_at < Time.now-1.minutes
-      @score = 1
-    elsif latest_comment.created_at > Time.now-1.minute
-      @score += 0
-    else @score += 2
-    end
+      if comments.any?
+        comments_score
+      elsif Time.now-2.days > Season.current.starts_at && Time.now < Season.current.ends_at
+        # Maybe we should change the +3 score temporarily for the upcoming week,
+        # because before Wed 16th comments-without-text were not being saved.
+        @score += 3
+      else
+        @score += 0
+      end
+
+      if activities.any?
+        activity_score
+      elsif Time.now-2.days > Season.current.starts_at && Time.now < Season.current.ends_at
+        @score += 3
+      else
+        @score += 0
+      end
     evaluate_performance
+  end
+
+  def comments_score
+    latest_comment = comments.ordered.first
+    if latest_comment.created_at < Time.now-5.days
+      @score += 2
+    elsif latest_comment.created_at < Time.now-2.days
+      @score += 1
+    elsif latest_comment.created_at > Time.now-2.days
+      @score += 0
+    else @score += 1
+    end
+  end
+
+  def activity_score
+    if last_activity.created_at < Time.now-5.days
+      @score += 2
+    elsif last_activity.created_at < Time.now-3.days
+      @score += 1
+    elsif last_activity.created_at > Time.now-3.days
+      @score += 0
+      else @score += 1
+    end
   end
 
   def evaluate_performance
     @performance = ' '
-    if @score == 0
-      @performance = "Green"
-    elsif @score == 1
-      @performance = "Orange"
-    else
+    if @score > 3
       @performance = "Red"
+    elsif @score >= 2
+      @performance = "Orange"
+    elsif @score == 0
+      @performance = "Green"
+    else @performance = "Orange"
     end
   end
 
