1dc281a7d163a27649dbd2c55989dafd602fd932
F3PiX <F3PiXnl@gmail.com>
2015-09-28 20:24:54 +0200

    Adds alphabetical order to teams on Dashboard

--- app/controllers/supervisor/dashboard_controller.rb
+++ app/controllers/supervisor/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class Supervisor::DashboardController < Supervisor::BaseController
+  before_action :order, only: :index
 
   def index
+  end
+
+  private
+
+  def order
+    params[:name] if params[:name]
   end
 
 end
