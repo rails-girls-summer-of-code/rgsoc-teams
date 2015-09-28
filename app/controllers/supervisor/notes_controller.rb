d5a33ca7a38d40e423e93d0d0c1b55faef032f71
F3PiX <F3PiXnl@gmail.com>
2015-09-28 20:27:43 +0200

    Indentation...

--- app/controllers/supervisor/notes_controller.rb
+++ app/controllers/supervisor/notes_controller.rb
@@ -3,7 +3,7 @@ class Supervisor::NotesController < Supervisor::BaseController
   def update
     @notepad = @current_user.notepad
     @notepad.update(notepad_params)
-      redirect_to supervisor_dashboard_path
+    redirect_to supervisor_dashboard_path
   end
 
   private
