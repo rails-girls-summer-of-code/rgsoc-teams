9ad1511f84a3d7a1627a34f8c3c752c93337ac3f
F3PiX <F3PiXnl@gmail.com>
2015-09-29 12:54:09 +0200

    Adds index to user_id in notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928180532) do
+ActiveRecord::Schema.define(version: 20150929104102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,8 @@ ActiveRecord::Schema.define(version: 20150928180532) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
     t.string   "name",       limit: 255
