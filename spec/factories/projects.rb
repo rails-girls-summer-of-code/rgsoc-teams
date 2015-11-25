40ea1e275f3e0b45ea72cd784242fc9b2351346c
Carsten Zimmermann <cz@aegisnet.de>
2015-11-25 23:39:12 +0100

    Create factories for all states

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
   factory :project do
     name { FFaker::Product.product_name }
     association :submitter, factory: :user
+
+    trait :accepted do
+      after(:create) { |record| record.accept! }
+    end
+
+    trait :rejected do
+      after(:create) { |record| record.reject! }
+    end
   end
 end
