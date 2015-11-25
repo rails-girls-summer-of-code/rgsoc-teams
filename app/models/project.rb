0eaad095286f6f9957a26165d232ba07cc5fc15b
Carsten Zimmermann <cz@aegisnet.de>
2015-11-25 21:43:26 +0100

    Add a state machine

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,21 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
+
+  include AASM
+
+  aasm do
+    state :proposed, initial: true
+    state :accepted
+    state :rejected
+
+    event :accept do
+      transitions from: :proposed, to: :accepted
+    end
+
+    event :reject do
+      transitions from: :proposed, to: :rejected
+    end
+
+  end
+
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
-describe Project do
+RSpec.describe Project do
   it { is_expected.to validate_presence_of(:name) }
+
+  context 'as a finite state machine' do
+    it 'starts as "proposed"' do
+      expect(subject).to be_proposed
+    end
+
+    context 'with a proposed project' do
+      subject { create :project }
+
+      it 'can be accepted' do
+        expect(subject).to be_may_accept
+        expect { subject.accept! }.to \
+          change { subject.accepted? }.to true
+      end
+
+      it 'can be rejected' do
+        expect(subject).to be_may_reject
+        expect { subject.reject! }.to \
+          change { subject.rejected? }.to true
+      end
+
+    end
+
+  end
 end
