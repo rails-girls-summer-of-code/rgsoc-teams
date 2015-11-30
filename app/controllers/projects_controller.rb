6ba7c7bef2b470e22518a2da7f71863de6b97325
ramon huidobro <ramon.martin.huidobro@gmail.com>
2015-11-30 22:48:26 +0100

    Implement post create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,4 +10,25 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def create
+    @project = Project.new(project_params)
+
+    @project.submitter_id = current_user.id
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: 'Project was successfully submitted.' }
+      else
+        format.html { render action: :new }
+      end
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :name, :mentor_name, :mentor_github_handle, :mentor_email,
+      :url, :description, :issues_and_features, :beginner_friendly
+    )
+  end
 end
