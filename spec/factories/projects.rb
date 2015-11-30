ca7d0ab0a7ad74f8d200c7966b526b73317c719c
ramon huidobro <ramon.martin.huidobro@gmail.com>
2015-11-30 22:46:27 +0100

    Add fields to generated project

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,7 +4,13 @@ FactoryGirl.define do
   factory :project do
     name { FFaker::Product.product_name }
     association :submitter, factory: :user
-
+    mentor_name { FFaker::Product.product_name }
+    mentor_github_handle { FFaker::Internet.user_name }
+    mentor_email { FFaker::Internet.email }
+    url { FFaker::Internet.http_url }
+    description { FFaker::HipsterIpsum.paragraph }
+    issues_and_features { FFaker::Internet.email }
+    beginner_friendly true
     trait :accepted do
       after(:create) { |record| record.accept! }
     end
