febd11f8cf02d7527ba057eb14d2af1f41349769
Carsten Zimmermann <cz@aegisnet.de>
2015-12-06 15:01:44 +0100

    Show project's tags

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,4 +11,9 @@ module ProjectsHelper
                   end
     content_tag :span, project.aasm_state, class: "label #{label_class}"
   end
+
+  def project_tags(project)
+    project.tags.map{ |t| "<span class='label label-default'>#{t}</span>" }.join(' ').html_safe
+  end
+
 end
