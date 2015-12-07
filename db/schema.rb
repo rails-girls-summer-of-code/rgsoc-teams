15942068d34e3ee26d2a48d3b2a6d5742cc78aff
Carsten Zimmermann <cz@aegisnet.de>
2015-12-07 21:02:58 +0100

    Add projects.comments_locked db column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207124423) do
+ActiveRecord::Schema.define(version: 20151207195550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,7 @@ ActiveRecord::Schema.define(version: 20151207124423) do
     t.string   "aasm_state"
     t.text     "tags",                             default: [], array: true
     t.string   "source_url"
+    t.boolean  "comments_locked",                  default: false
   end
 
   create_table "ratings", force: :cascade do |t|
