b4bb966b658473d815407712a132f4ea72a16c0f
F3PiX <F3PiXnl@gmail.com>
2016-02-09 22:05:42 +0100

    Improvements
    in specs format and date formats

--- app/models/season.rb
+++ app/models/season.rb
@@ -50,33 +50,33 @@ class Season < ActiveRecord::Base
   # between time dependent settings in views
   def fake_proposals_phase
     update({
-         starts_at: Date.today+6.months,
-         ends_at: Date.today+9.months,
-         applications_open_at: Date.today+3.months,
-         applications_close_at: Date.today+4.months,
-         acceptance_notification_at: Date.today+5.months,
-         project_proposals_open_at: Date.today-4.weeks,
-         project_proposals_close_at: Date.today+4.weeks
+         starts_at: 6.months.from_now,
+         ends_at: 9.months.from_now,
+         applications_open_at: 3.months.from_now,
+         applications_close_at: 4.months.from_now,
+         acceptance_notification_at: 5.months.from_now,
+         project_proposals_open_at: 4.weeks.ago,
+         project_proposals_close_at: 4.weeks.from_now
     })
   end
 
   def fake_application_phase
     update({
-        starts_at: Date.today+2.months,
-        ends_at: Date.today+5.months,
-        applications_open_at: Date.today-2.weeks,
-        applications_close_at: Date.today+2.weeks,
-        acceptance_notification_at: Date.today+6.weeks
+        starts_at: 2.months.from_now,
+        ends_at: 5.months.from_now,
+        applications_open_at: 2.weeks.ago,
+        applications_close_at: 2.weeks.from_now,
+        acceptance_notification_at: 6.weeks.from_now
     })
   end
 
   def fake_coding_phase
     update({
-        starts_at: Date.today-6.weeks,
-        ends_at: Date.today+6.weeks,
-        applications_open_at: Date.today-4.months,
-        applications_close_at: Date.today-3.months,
-        acceptance_notification_at: Date.today-2.months
+        starts_at: 6.weeks.ago,
+        ends_at: 6.weeks.from_now,
+        applications_open_at: 4.months.ago,
+        applications_close_at: 3.months.ago,
+        acceptance_notification_at: 2.months.ago
     })
   end
 
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -191,28 +191,34 @@ describe Season do
   end
 
   ######## Testing Season Switch #############
-  describe 'switch phase' do
-    it 'timeshifts the application phase to today' do
-      Timecop.travel(Season.current.applications_close_at - 1.month) do
-        subject.fake_application_phase
-        expect(subject).to be_application_period
+  context 'switching phases' do
+    describe '#fake_application_phase' do
+      it 'timeshifts the application phase to today' do
+        Timecop.travel(Season.current.applications_close_at - 1.month) do
+          subject.fake_application_phase
+          expect(subject).to be_application_period
+        end
       end
     end
 
-    it 'timeshifts the coding phase to today' do
-      Timecop.travel(Season.current.ends_at - 1.week) do
-        subject.fake_coding_phase
-        expect(subject).to be_started
+    describe '#fake_coding_phase' do
+      it 'timeshifts the coding phase to today' do
+        Timecop.travel(Season.current.ends_at - 1.week) do
+          subject.fake_coding_phase
+          expect(subject).to be_started
+        end
       end
     end
 
-    it 'timeshifts the proposal period to today' do
-      Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
-        fake_time = Time.now #as returned by Timecop
-        subject.fake_proposals_phase
-        expect(subject.project_proposals_open_at).to be < fake_time
-        expect(subject.project_proposals_close_at).to be > fake_time
-       end
+    describe '#fake_proposals_phase' do
+      it 'timeshifts the proposal period to today' do
+        Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
+          fake_time = Time.now #as returned by Timecop
+          subject.fake_proposals_phase
+          expect(subject.project_proposals_open_at).to be < fake_time
+          expect(subject.project_proposals_close_at).to be > fake_time
+        end
+      end
     end
   end
 
