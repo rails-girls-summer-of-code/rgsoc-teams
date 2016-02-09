d12446ad99ebbc6c7722df8cef9b940ac684e4b4
F3PiX <F3PiXnl@gmail.com>
2016-02-09 20:42:57 +0100

    Update tests for Season Switch
    - Corrects incorrect test for fake_coding_phase
    - Adds test for newly added Proposals_phase

--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -194,16 +194,25 @@ describe Season do
   describe 'switch phase' do
     it 'timeshifts the application phase to today' do
       Timecop.travel(Season.current.applications_close_at - 1.month) do
-        shift = subject.fake_application_phase
+        subject.fake_application_phase
         expect(subject).to be_application_period
       end
     end
 
     it 'timeshifts the coding phase to today' do
-      Timecop.travel(Season.current.applications_close_at - 1.day) do
-        shift = subject.fake_coding_phase
+      Timecop.travel(Season.current.ends_at - 1.week) do
+        subject.fake_coding_phase
         expect(subject).to be_started
       end
+    end
+
+    it 'timeshifts the proposal period to today' do
+      Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
+        fake_time = Time.now #as returned by Timecop
+        subject.fake_proposals_phase
+        expect(subject.project_proposals_open_at).to be < fake_time
+        expect(subject.project_proposals_close_at).to be > fake_time
+       end
     end
   end
 
