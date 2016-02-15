939be183661ffac14f3613018452b7b4fa511230
Carsten Zimmermann <cz@aegisnet.de>
2016-02-15 23:29:31 +0100

    Add foreign keys for projects to application_drafts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210124328) do
+ActiveRecord::Schema.define(version: 20160215222000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,12 @@ ActiveRecord::Schema.define(version: 20160210124328) do
     t.text     "project_plan"
     t.integer  "position"
     t.integer  "signed_off_by"
+    t.integer  "project1_id"
+    t.integer  "project2_id"
   end
 
+  add_index "application_drafts", ["project1_id"], name: "index_application_drafts_on_project1_id", using: :btree
+  add_index "application_drafts", ["project2_id"], name: "index_application_drafts_on_project2_id", using: :btree
   add_index "application_drafts", ["season_id"], name: "index_application_drafts_on_season_id", using: :btree
   add_index "application_drafts", ["signed_off_by"], name: "index_application_drafts_on_signed_off_by", using: :btree
   add_index "application_drafts", ["team_id"], name: "index_application_drafts_on_team_id", using: :btree
