4b6af60745101309e4a0a15ab047bb6862fa2bb0
Carsten Zimmermann <cz@aegisnet.de>
2016-03-12 01:40:28 +0100

    Inform coaches that they need to confirm themselves

--- lib/mailer_previews/role_mailer_preview.rb
+++ lib/mailer_previews/role_mailer_preview.rb
@@ -2,4 +2,8 @@ class RoleMailerPreview < ActionMailer::Preview
   def user_added_to_team
     RoleMailer.user_added_to_team(Role.first)
   end
+
+  def coach_added_to_team
+    RoleMailer.user_added_to_team(Role.where(name: 'coach').first)
+  end
 end
