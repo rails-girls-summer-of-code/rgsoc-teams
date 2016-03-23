11797fe59a6fc4081c099d99b48f59f105e8d11b
Pilar Huidobro <huidobropilar@gmail.com>
2016-03-23 21:29:55 +0100

    Filter teams not from curr. season when reviewing

--- app/controllers/applications/todos_controller.rb
+++ app/controllers/applications/todos_controller.rb
@@ -8,6 +8,7 @@ class Applications::TodosController < ApplicationController
   def index
     @teams = Team.joins(:applications)
                  .where('"applications_count" > 0')
+                 .where(season: current_season)
                  .includes(:students, :applications)
   end
 end
--- spec/controllers/applications/todos_controller_spec.rb
+++ spec/controllers/applications/todos_controller_spec.rb
@@ -6,22 +6,28 @@ describe Applications::TodosController, type: :controller do
   describe 'GET #index' do
     let(:role) { FactoryGirl.create(:reviewer_role, team: nil) }
     let(:current_user) { role.user }
-    let!(:teams) { FactoryGirl.create_list(:team, 2, :applying_team, :with_applications) }
+    let!(:teams) { FactoryGirl.create_list(:team, 2, :applying_team, :with_applications, :current_season) }
     let!(:team_without_application) { FactoryGirl.create(:team) }
     let!(:team_rating) { teams.sample.ratings.create(user: current_user) }
     let!(:student_rating) { teams.sample.students.sample.ratings.create(user: current_user) }
     let!(:application_rating) { teams.sample.applications.sample.ratings.create(user: current_user) }
+    let!(:team_with_application_no_season) { FactoryGirl.create(:team, :applying_team, :with_applications) }
 
     subject { get :index }
 
     before do
       allow(controller).to receive(:authenticate_user!)
       allow(controller).to receive(:current_user).and_return(current_user)
+      allow(controller).to receive(:current_season).and_return(Season.current)
       subject
     end
 
     it 'assigns @teams' do
       expect(assigns(:teams)).to match_array(teams)
+    end
+
+    it '@teams does not include teams that aren\'t from the current season' do
+      expect(assigns(:teams)).to_not include(team_with_application_no_season)
     end
 
     it '@teams does not include teams without applications' do
