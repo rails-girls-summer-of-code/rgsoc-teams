c5d252038c6750731d1bb003784961f02a79be67
Max Mulatz <klappradla@gmail.com>
2016-04-27 23:57:39 +0200

    add tooltip helpers for each rating criteria
    (defined in config/tooltips.yml file)

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -3,4 +3,27 @@ module RatingsHelper
     points = subject.try(:average_points) || subject.try(:points)
     points.round precision if points
   end
+
+  def field_tooltip(key)
+    key = key.to_s
+    { data:
+      { toggle: 'popover',
+        placement: 'left',
+        container: 'body',
+        trigger: 'hover',
+        content: tooltip_text_for(key),
+        html: true },
+      title: key.titleize }
+  end
+
+  private
+
+  def tooltip_text_for(key)
+    key = key.gsub(/_student_\d/, '')
+    simple_format tooltips[key]
+  end
+
+  def tooltips
+    @tooltips ||= YAML.load_file('config/tooltips.yml')
+  end
 end
