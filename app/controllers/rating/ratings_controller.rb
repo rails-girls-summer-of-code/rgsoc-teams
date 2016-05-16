fa14131eea568cd26119f77b5412a5b498aba7a7
Michael Emhofer <michael@emhofer.net>
2016-05-16 17:53:21 +0200

    consistency between ratings and comments controllers

--- app/controllers/rating/ratings_controller.rb
+++ app/controllers/rating/ratings_controller.rb
@@ -1,25 +1,21 @@
 class Rating::RatingsController < Rating::BaseController
-  before_filter :set_redirect_path, only: [:create, :update]
+  PATH_PARENTS = [:rating, :applications]
 
   # In order to get the rating data persisted, FIRST create the rating record,
   # then update it to actually set the values.
   def create
     rating = find_or_create_rating
     rating.update new_rating_params
-    redirect_to @redirect_path
+    redirect_to self.class::PATH_PARENTS
   end
 
   def update
     rating = Rating.by(current_user).find(params[:id])
     rating.update(rating_attr_params)
-    redirect_to @redirect_path
+    redirect_to self.class::PATH_PARENTS
   end
 
   private
-    def set_redirect_path
-      @redirect_path = rating_applications_path
-    end
-
     def new_rating_params
       params.require(:rating).permit(:rateable_id, :rateable_type, :pick, Rating::FIELDS.keys)
     end
--- app/controllers/rating/todos/ratings_controller.rb
+++ app/controllers/rating/todos/ratings_controller.rb
@@ -1,7 +1,3 @@
 class Rating::Todos::RatingsController < Rating::RatingsController
-
-  private
-    def set_redirect_path
-      @redirect_path = rating_todos_path
-    end
+  PATH_PARENTS = [:rating, :todos]
 end
