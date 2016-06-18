5d86e526c4e8c32ba5b835a19f60ff047a8fd297
Carsten Zimmermann <cz@aegisnet.de>
2016-06-18 10:56:32 +0200

    Add teams exporter

---
+++ app/exporters/teams.rb
@@ -0,0 +1,15 @@
+module Exporters
+  class Teams < Base
+
+    def current
+      teams = Team.where(season: Season.current).includes(:members)
+
+      generate(teams, 'Team ID', 'Name', 'Location', "1st Student's Name", "1st Student's Email", "2nd Student's Name", "2nd Student's Email") do |t|
+        student1 = t.students[0] || Student.new
+        student2 = t.students[1] || Student.new
+        [t.id, t.name, t.students_location, student1.name, student1.email, student2.name, student2.email]
+      end
+    end
+
+  end
+end
---
+++ spec/exporters/teams_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.describe Exporters::Teams do
+
+  describe '#current' do
+    let!(:old_team) { create :team, name: "OLDTEAM" }
+    let!(:new_team) { create :team, :current_season, name: "NEWTEAM" }
+    let!(:app_team) { create :team, :applying_team, :current_season, name: "APPLYINGTEAM" }
+
+    subject { described_class.current }
+
+    it 'exports all teams of the current season' do
+      expect(subject).to match 'NEWTEAM'
+      expect(subject).to match 'APPLYINGTEAM'
+      expect(subject).not_to match 'OLDTEAM'
+    end
+  end
+
+end
+
