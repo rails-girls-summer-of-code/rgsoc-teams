3a6da8465a0c8778ccdbd55ff454fca8944bfc02
Carsten Zimmermann <cz@aegisnet.de>
2016-06-18 11:01:28 +0200

    Include coaches' emails

--- app/exporters/teams.rb
+++ app/exporters/teams.rb
@@ -4,10 +4,11 @@ module Exporters
     def current
       teams = Team.where(season: Season.current).includes(:members)
 
-      generate(teams, 'Team ID', 'Name', 'Location', "1st Student's Name", "1st Student's Email", "2nd Student's Name", "2nd Student's Email") do |t|
+      generate(teams, 'Team ID', 'Name', 'Location', "1st Student's Name", "1st Student's Email", "2nd Student's Name", "2nd Student's Email", "Coaches' Emails") do |t|
         student1 = t.students[0] || Student.new
         student2 = t.students[1] || Student.new
-        [t.id, t.name, t.students_location, student1.name, student1.email, student2.name, student2.email]
+        coaches_emails = t.coaches.map(&:email).reject(&:blank?).join(', ')
+        [t.id, t.name, t.students_location, student1.name, student1.email, student2.name, student2.email, coaches_emails]
       end
     end
 
