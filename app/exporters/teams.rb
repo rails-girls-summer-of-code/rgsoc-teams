28c2c60a81dc0e488b1f6fc51b5c1045565b26da
Carsten Zimmermann <cz@aegisnet.de>
2016-06-20 09:15:01 +0200

    Add coaches' names
    Touch #467

--- app/exporters/teams.rb
+++ app/exporters/teams.rb
@@ -4,11 +4,12 @@ module Exporters
     def current
       teams = Team.where(season: Season.current).includes(:members)
 
-      generate(teams, 'Team ID', 'Name', 'Location', "1st Student's Name", "1st Student's Email", "2nd Student's Name", "2nd Student's Email", "Coaches' Emails") do |t|
+      generate(teams, 'Team ID', 'Name', 'Location', "1st Student's Name", "1st Student's Email", "2nd Student's Name", "2nd Student's Email", "Coaches' Emails", "Coaches' Names") do |t|
         student1 = t.students[0] || Student.new
         student2 = t.students[1] || Student.new
         coaches_emails = t.coaches.map(&:email).reject(&:blank?).join(', ')
-        [t.id, t.name, t.students_location, student1.name, student1.email, student2.name, student2.email, coaches_emails]
+        coaches_names  = t.coaches.map(&:name_or_handle).join(', ')
+        [t.id, t.name, t.students_location, student1.name, student1.email, student2.name, student2.email, coaches_emails, coaches_names]
       end
     end
 
