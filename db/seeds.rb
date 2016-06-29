07b42a4b330151f0aead55a3cfe84d4112eae626
F3PiX <F3PiXnl@gmail.com>
2016-06-30 00:19:09 +0200

    More and better seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+FactoryGirl.create_list(:team, 5, :current_season, kind: "sponsored")
+FactoryGirl.create(:team, :current_season, kind: "voluntary")
+
+
 FactoryGirl.create_list(:student, 12)
 FactoryGirl.create_list(:coach, 3)
 FactoryGirl.create_list(:organizer, 2)
@@ -5,7 +9,17 @@ FactoryGirl.create(:mentor)
 FactoryGirl.create(:supervisor)
 
 # To explore use cases where user has no role yet
-# NB These are not listed in the Community listing
-FactoryGirl.create_list(:user, 6)
+FactoryGirl.create_list(:user, 3)
 
-FactoryGirl.create_list(:status_update, 5, published_at: Time.now)
+#Create status updates for different teams
+5.times do
+FactoryGirl.create(:status_update, published_at: Time.now, team_id: (0..Team.all.count).to_a.sample)
+end
+
+
+FactoryGirl.create(:application_draft)
+FactoryGirl.create(:application_draft, :appliable)
+
+FactoryGirl.create(:project) #proposed
+FactoryGirl.create(:project, :accepted)
+FactoryGirl.create(:project, :rejected)
