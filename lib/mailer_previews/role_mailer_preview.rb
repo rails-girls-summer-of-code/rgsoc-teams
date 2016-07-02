60b1a6d05ce80419ccaa38fc3712d2cc341c4d38
Carsten Zimmermann <cz@aegisnet.de>
2016-07-02 10:05:46 +0200

    Add preview for new supervisor role mail notification

--- lib/mailer_previews/role_mailer_preview.rb
+++ lib/mailer_previews/role_mailer_preview.rb
@@ -1,3 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/role_mailer
 class RoleMailerPreview < ActionMailer::Preview
   def user_added_to_team
     RoleMailer.user_added_to_team(Role.first)
@@ -5,5 +6,10 @@ class RoleMailerPreview < ActionMailer::Preview
 
   def coach_added_to_team
     RoleMailer.user_added_to_team(Role.where(name: 'coach').first)
+  end
+
+  def supervisor_added_to_team
+    role = Role.where(name: 'supervisor').first
+    RoleMailer.user_added_to_team role
   end
 end
