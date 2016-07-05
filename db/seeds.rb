e224f60aa872654bc23036b0e56790d15f91edb3
F3PiX <F3PiXnl@gmail.com>
2016-07-05 15:51:04 +0200

    Add unaccepted team

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
+# Teams
 FactoryGirl.create_list(:team, 5, :current_season, kind: "sponsored")
 FactoryGirl.create(:team, :current_season, kind: "voluntary")
+FactoryGirl.create (:team, :current_season) #not accepted
+
 FactoryGirl.create(:team, :last_season, kind: "sponsored")
 FactoryGirl.create(:team, :last_season, kind: "voluntary")
 
+# Users with different roles
 FactoryGirl.create_list(:student, 12)
 FactoryGirl.create_list(:coach, 3)
 FactoryGirl.create_list(:organizer, 2)
@@ -12,12 +16,13 @@ FactoryGirl.create(:supervisor)
 # To explore use cases where user has no role yet
 FactoryGirl.create_list(:user, 3)
 
-#Create status updates for different teams
+
+# Status updates for different teams
 5.times do
   FactoryGirl.create(:status_update, published_at: Time.now, team: Team.all.sample)
 end
 
-
+# Applications and their projects
 FactoryGirl.create(:application_draft)
 FactoryGirl.create(:application_draft, :appliable)
 
