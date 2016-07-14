c1fcba2bd6c0a61d46dd4de0148b8cd20f3bc3cf
F3PiX <F3PiXnl@gmail.com>
2016-07-14 12:08:53 +0200

    Refactor Team scope from current into in_current_season
    Step 1: the Team.in_current_season scope

--- app/controllers/orga/teams_controller.rb
+++ app/controllers/orga/teams_controller.rb
@@ -11,7 +11,7 @@ class Orga::TeamsController < Orga::BaseController
       @teams = Team.order(:kind, :name)
     end
     if params[:filter] != 'all'
-      @teams = Team.current.selected.ordered
+      @teams = Team.in_current_season.selected.ordered
     end
   end
 
