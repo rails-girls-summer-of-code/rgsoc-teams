f9aeaa005af615d194e6bf82011bce4302ce929f
Carsten Zimmermann <cz@aegisnet.de>
2016-07-19 00:32:51 +0200

    Fix comment form for rating applications

---
+++ spec/controllers/rating/todos/applications_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe Rating::Todos::ApplicationsController do
+  render_views
+
+  let(:application) { create :application }
+
+  describe 'GET show' do
+    it 'requires login' do
+      get :show, id: application.to_param
+      expect(response).to redirect_to root_path
+      expect(flash[:alert]).to be_present
+    end
+
+    it 'requires reviewer role' do
+      sign_in create(:organizer)
+      get :show, id: application.to_param
+      expect(response).to redirect_to root_path
+      expect(flash[:alert]).to be_present
+    end
+
+    context 'when reviewer' do
+      let(:user) { create :reviewer }
+
+      before do
+        sign_in user
+        get :show, id: application.to_param
+      end
+
+      it 'finds and assigns @application by id param' do
+        expect(assigns :application).to eql application
+      end
+
+      it 'renders the "show" template' do
+        expect(response).to render_template 'rating/todos/applications/show'
+      end
+    end
+  end
+end
