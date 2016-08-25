93414a540ae6efcae57c270aa66dc88ff9374c45
alicetragedy <girl@alicetragedy.org>
2016-08-25 18:04:12 +0200

    Retrieve current season students in json format to list on rgsoc website

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+  after_action :cors_set_headers, only: :index
+
+  respond_to :json
+
+  def index
+    @students = User.with_role("student").includes(roles: :team).
+      where("teams.season_id = ?", Season.current).
+      references(:teams).uniq
+    respond_with @students.as_json(json_params)
+  end
+
+  private
+
+  def json_params
+    {
+      only: [:github_handle, :avatar_url, :location, :country, :twitter_handle],
+      methods: [:name_or_handle],
+      include: [
+        roles: {
+          only: [:name],
+          include: [team: { only: [:id, :name, :kind] }]
+        }
+      ]
+    }
+  end
+end
