0c7535958b8911defb24cd0c647d70cdb7c639b7
Carsten Zimmermann <cz@aegisnet.de>
2016-09-22 22:11:53 +0200

    Dynamicall extract season from method_missing call

--- app/exporters/users.rb
+++ app/exporters/users.rb
@@ -5,6 +5,19 @@ module Exporters
       export_for_season(Season.current)
     end
 
+    def respond_to_missing?(meth, *)
+      !!(meth.to_s =~ %r(\Astudents_\d{4}\z))
+    end
+
+    def method_missing(meth, *args, &block)
+      if matches = %r(\Astudents_(?<year>\d{4})\z).match(meth.to_s)
+        season = Season.find_by(name: matches[:year])
+        export_for_season(season)
+      else
+        super
+      end
+    end
+
     private
 
     def export_for_season(season)
--- spec/exporters/users_spec.rb
+++ spec/exporters/users_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe Exporters::Users do
     end
   end
 
+  describe '#students_<year>' do
+    let(:old_season) { create :season, name: %w(2014 2015).sample }
+
+    let(:old_team) { create :team, season: old_season }
+    let(:new_team) { create :team, :in_current_season }
+
+    let!(:old_student) { create :user, name: "OLDSTUDENT" }
+    let!(:new_student) { create :user, name: "NEWSTUDENT" }
+
+    before do
+      create :student_role, user: old_student, team: old_team
+      create :student_role, user: new_student, team: new_team
+    end
+
+    it 'exports all projects of the current season' do
+      export_method = "students_#{old_season.year}"
+      expect(described_class.send export_method).to match 'OLDSTUDENT'
+      expect(described_class.send export_method).not_to match 'NEWSTUDENT'
+    end
+
+    it 'will not raise an exception when trying to export a nonexistent season' do
+      export_method = "students_1970"
+      expect(described_class.send export_method).not_to match 'OLDSTUDENT'
+      expect(described_class.send export_method).not_to match 'NEWSTUDENT'
+    end
+
+  end
+
 end
