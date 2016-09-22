3a20e70d3839921cdf43e272a5d68691b83a8187
Carsten Zimmermann <cz@aegisnet.de>
2016-09-22 22:39:35 +0200

    Export old accepted teams

--- spec/exporters/teams_spec.rb
+++ spec/exporters/teams_spec.rb
@@ -16,5 +16,26 @@ RSpec.describe Exporters::Teams do
     end
   end
 
+  describe '#accepted_<year>' do
+    let(:old_season) { create :season, name: %w(2014 2015).sample }
+
+    let!(:old_team) { create :team, season: old_season, name: "OLDTEAM" }
+    let!(:new_team) { create :team, :in_current_season, name: "NEWTEAM" }
+    let!(:app_team) { create :team, :applying_team, season: old_season, name: "APPLYINGTEAM" }
+
+    it 'exports all accepted teams of the given season' do
+      export_method = "accepted_#{old_season.year}"
+      expect(described_class.send export_method).to match 'OLDTEAM'
+      expect(described_class.send export_method).not_to match 'NEWSTUDENT'
+      expect(described_class.send export_method).not_to match 'APPLYINGTEAM'
+    end
+
+    it 'will raise an exception when trying to export a nonexistent season' do
+      export_method = "accepted_1970"
+      expect { described_class.send export_method }.to raise_error NoMethodError
+    end
+
+  end
+
 end
 
