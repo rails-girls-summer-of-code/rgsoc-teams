77cd79b44ccd635265b86d558f47766dbe66a489
Carsten Zimmermann <cz@aegisnet.de>
2016-09-22 21:53:10 +0200

    Make export a function of season

--- app/exporters/users.rb
+++ app/exporters/users.rb
@@ -2,13 +2,19 @@ module Exporters
   class Users < Base
 
     def current_students
+      export_for_season(Season.current)
+    end
+
+    private
+
+    def export_for_season(season)
       students = User.with_role('student').with_team_kind(%w(sponsored voluntary)).
-        where("teams.season_id" => Season.current)
+        where("teams.season_id" => season)
 
       generate(students, 'User ID', 'Name', 'Email', 'Country', 'Locality', 'Address', 'T-shirt size') do |u|
         [u.id, u.name, u.email, u.country, u.location, u.postal_address, u.tshirt_size]
       end
-
     end
+
   end
 end
