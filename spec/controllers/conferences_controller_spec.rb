1c54d09d31cad1305e867205380e61bba46877ac
F3PiX <f3pixnl@gmail.com>
2016-10-11 22:12:00 +0200

    Remove comments

--- spec/controllers/conferences_controller_spec.rb
+++ spec/controllers/conferences_controller_spec.rb
@@ -15,35 +15,4 @@ RSpec.describe ConferencesController do
     end
 
   end
-
-  # context 'with admin logged in' do
-  #   include_context 'with admin logged in'
-  #
-  #   describe 'POST create' do
-  #     it 'creates a record' do
-  #       expect {
-  #         post :create, conference: attributes_for(:conference)
-  #       }.to change { Conference.count }.by 1
-  #       expect(response).to redirect_to Conference.last
-  #     end
-  #
-  #     it 'sets the current season' do
-  #       post :create, conference: attributes_for(:conference)
-  #       expect(assigns(:conference).season.name).to eql Date.today.year.to_s
-  #     end
-  #   end
-  #
-  #   describe 'DELETE destroy' do
-  #     let!(:conference) { create :conference }
-  #
-  #     it 'destroys the resource' do
-  #       expect {
-  #         delete :destroy, id: conference
-  #       }.to change { Conference.count }.by(-1)
-  #       expect(response).to redirect_to conferences_path
-  #     end
-  #
-  #   end
-  # end
-
 end
