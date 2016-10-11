3acf4a8a6aaf24907bec0c94d0f49f8e8ca31d48
F3PiX <f3pixnl@gmail.com>
2016-10-11 20:51:34 +0200

    WIP Move conferences CRUD to Orga namespace
    - Add conferences_controller to orga namespace
    - Remove CRUD actions from conferences_controller
    - Add _table partial for shared views

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,22 +1,6 @@
 class ConferencesController < ApplicationController
   load_and_authorize_resource except: [:index, :show]
 
-  def create
-    conference.season = current_season
-    conference.save!
-    redirect_to conference
-  end
-
-  def update
-    conference.update_attributes(conference_params)
-    redirect_to conference
-  end
-
-  def destroy
-    conference.destroy!
-    redirect_to conferences_path, notice: 'The conference has been deleted.'
-  end
-
   private
 
     def conferences
@@ -28,15 +12,6 @@ class ConferencesController < ApplicationController
       @conference ||= params[:id] ? Conference.find(params[:id]) : Conference.new(conference_params)
     end
     helper_method :conference
-
-    def conference_params
-      params[:conference] ? params.require(:conference).permit(
-        :name, :url, :location, :twitter, :tickets, :flights, :accomodation,
-        :'starts_on(1i)', :'starts_on(2i)', :'starts_on(3i)', :round,
-        :'ends_on(1i)', :'ends_on(2i)', :'ends_on(3i)', :lightningtalkslots,
-        attendances_attributes: [:id, :github_handle, :_destroy]
-      ) : {}
-    end
 
     def sort_params
       {
---
+++ app/controllers/orga/conferences_controller.rb
@@ -0,0 +1,51 @@
+class Orga::ConferencesController < Orga::BaseController
+  load_and_authorize_resource except: [:index, :show]
+
+  def show
+    render 'conferences/show'
+  end
+
+  def create
+    conference.season = current_season
+    conference.save!
+    redirect_to orga_conference_path(conference)
+  end
+
+  def update
+    conference.update_attributes(conference_params)
+    redirect_to orga_conference_path(conference)
+  end
+
+  def destroy
+    conference.destroy!
+    redirect_to orga_conferences_path, notice: 'The conference has been deleted.'
+  end
+
+  private
+
+    def conferences
+      @conferences ||= Conference.ordered(sort_params).in_current_season
+    end
+    helper_method :conferences
+
+    def conference
+      @conference ||= params[:id] ? Conference.find(params[:id]) : Conference.new(conference_params)
+    end
+    helper_method :conference
+
+    def conference_params
+      params[:conference] ? params.require(:conference).permit(
+        :name, :url, :location, :twitter, :tickets, :flights, :accomodation,
+        :'starts_on(1i)', :'starts_on(2i)', :'starts_on(3i)', :round,
+        :'ends_on(1i)', :'ends_on(2i)', :'ends_on(3i)', :lightningtalkslots,
+        attendances_attributes: [:id, :github_handle, :_destroy]
+      ) : {}
+    end
+
+    def sort_params
+      {
+        order: %w(name location starts_on).include?(params[:sort]) ? params[:sort] : nil,
+        direction: %w(asc desc).include?(params[:direction]) ? params[:direction] : nil
+      }
+    end
+end
