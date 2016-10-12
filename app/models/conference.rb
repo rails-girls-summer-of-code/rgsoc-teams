50d659c38edb806d58e94752aac9eb80e682e891
F3PiX <f3pixnl@gmail.com>
2016-10-12 20:59:27 +0200

    Remove end_date validation
    It rejects ALL end dates.

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -5,16 +5,11 @@ class Conference < ActiveRecord::Base
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
   validates :name, :round, presence: true
-  validate :valid_end_date, on: :create
 
   accepts_nested_attributes_for :attendances
 
   scope :ordered, ->(sort = {}) { order([sort[:order] || 'starts_on, name', sort[:direction] || 'asc'].join(' ')) }
   scope :in_current_season, -> { where(season: Season.current) }
-
-  def valid_end_date
-    errors.add(:ends_on, "-date must be after start date") unless :ends_on >= :starts_on
-  end
 
   def tickets_left
     confirmed_attendances = attendances.select { |attendance| attendance.confirmed }
