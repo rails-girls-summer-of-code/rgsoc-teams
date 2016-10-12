8658d8f45a260c1653847ba3b2d56ce373c774da
Carsten Zimmermann <cz@aegisnet.de>
2016-10-12 22:30:54 +0200

    Add spec for unteamed organizers

--- spec/controllers/contributors_controller_spec.rb
+++ spec/controllers/contributors_controller_spec.rb
@@ -7,8 +7,14 @@ describe ContributorsController do
     let!(:user)      { create(:user) }
     let!(:coach)     { create(:coach) }
     let!(:organizer) { create(:organizer) }
-    
+
     it 'returns json representation of user with relevant roles' do
+      get :index, format: :json
+      expect(assigns(:contributors).sort).to eq [coach, organizer].sort
+    end
+
+    it 'includes teamless organizers' do
+      organizer.roles.each { |r| r.update! team: nil }
       get :index, format: :json
       expect(assigns(:contributors).sort).to eq [coach, organizer].sort
     end
