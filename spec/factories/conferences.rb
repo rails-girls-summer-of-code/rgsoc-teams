d3c8d5f213a06bb1b2c8a18a1842c9503b5e8019
F3PiX <F3PiXnl@gmail.com>
2016-11-26 09:41:41 +0100

    Fix conference factory

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -1,14 +1,13 @@
 FactoryGirl.define do
-  factory :conference do
-    name { [FFaker::CheesyLingo.title, 'Conf'].join ' ' }
-    tickets 2
-    starts_on '2017-01-01'
-    ends_on '2017-02-02'
-    round 1
+    factory :conference do
+      name [FFaker::CheesyLingo.title, 'Conf'].join(' ')
+      tickets 2
+      starts_on { Time.utc(Date.today.year, 7, 7) }
+      ends_on { Time.utc(Date.today.year, 7, 15) }
+      round 1
 
     trait :in_current_season do
       season { Season.current }
     end
-
   end
 end
