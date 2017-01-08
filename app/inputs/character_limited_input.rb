35d0af74a74e167aa0174d5de8f63bd17ef0bec8
Carsten Zimmermann <cz@aegisnet.de>
2017-01-08 13:59:55 +0100

    Add a class

--- app/inputs/character_limited_input.rb
+++ app/inputs/character_limited_input.rb
@@ -11,7 +11,8 @@ class CharacterLimitedInput < SimpleForm::Inputs::TextInput
 
   def counter
     chars_left  = Student::CHARACTER_LIMIT - @builder.object.send(attribute_name).size
-    num_element = content_tag(:span, chars_left)
+    num_element = content_tag(:span, chars_left, class: 'character_limited_counter')
+
     content_tag(:p, "#{num_element} character(s) left".html_safe)
   end
 end
