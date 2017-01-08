2ac2e9ff3da009ef9f98616654a29350479b2502
Carsten Zimmermann <cz@aegisnet.de>
2017-01-08 13:33:27 +0100

    WIP: Add a counter DOM element

--- app/inputs/character_limited_input.rb
+++ app/inputs/character_limited_input.rb
@@ -2,4 +2,14 @@ class CharacterLimitedInput < SimpleForm::Inputs::TextInput
   def input_html_options
     super.merge(maxlength: Student::CHARACTER_LIMIT)
   end
+
+  def input(wrapper_options)
+    [super, counter].join("\n").html_safe
+  end
+
+  private
+
+  def counter
+    content_tag(:span, "0")
+  end
 end
