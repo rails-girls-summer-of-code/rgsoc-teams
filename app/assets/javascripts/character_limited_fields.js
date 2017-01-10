076f5c20456e5a69d76f18f19d53152c1c827e04
Carsten Zimmermann <cz@aegisnet.de>
2017-01-10 09:31:38 +0100

    Display error indicator

--- app/assets/javascripts/character_limited_fields.js
+++ app/assets/javascripts/character_limited_fields.js
@@ -1,14 +1,25 @@
 $(function() {
+    'use strict';
+
     $('[data-behaviour="character-limited"]').each(function() {
         $(this).on("keyup", characterCounter);
     });
 
     function characterCounter() {
         var $field = $(this);
-        var $count = $field.next().children('.character_limited_counter');
+        var $countParagraph = $field.next();
+        var $count = $countParagraph.children('.character_limited_counter');
 
         var maxlength      = parseInt($field.data('maxlength'));
         var remainingChars = maxlength - $field.val().length;
+
+        if (remainingChars < 0) {
+            if (!$countParagraph.hasClass('character_limit_exceeded')) {
+                $countParagraph.addClass('character_limit_exceeded');
+            }
+        } else {
+            $countParagraph.removeClass('character_limit_exceeded');
+        }
 
         $count.html(remainingChars);
     }
