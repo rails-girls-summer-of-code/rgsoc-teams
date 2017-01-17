f954bc7e55f67fcde56b1369f1b1b921e677c036
F3PiX <F3PiXnl@gmail.com>
2017-01-17 22:15:55 +0100

    Improve tests
    - With better understanding of what 'testing the public interface' means, changed private method tests into testing the public interface. (I hope)
    - Changed names and descriptions, to solve obscurities.

--- spec/models/season/phase_switcher_spec.rb
+++ spec/models/season/phase_switcher_spec.rb
@@ -12,45 +12,46 @@ context 'when switching phases' do
     end
 
     it 'addresses the requested phase' do
-      wrong_phase = :fake_proposals_phase
-      Season::PhaseSwitcher.destined(wrong_phase)
+      non_existing_phase = :fake_proposals_phase
+      Season::PhaseSwitcher.destined(non_existing_phase)
       expect(subject).to_not be_application_period
     end
 
     it 'fails silently when it receives a non-whitelisted phase' do
       phase = :bad_intentions
-      bad_call = Season::PhaseSwitcher.destined(phase)
-      expect( bad_call ).to be_nil
+      forbidden_call = Season::PhaseSwitcher.destined(phase)
+      expect { forbidden_call }.not_to change { Season.current.updated_at }
     end
-  end
 
-  describe '#fake_application_phase' do
-
-    it 'timeshifts the application phase to today' do
-      phase = :fake_application_phase
-      Timecop.travel(Season.current.applications_close_at - 1.month) do
-        Season::PhaseSwitcher.destined(phase)
-        expect(subject).to be_application_period
+    context 'when directed to the #fake_application_phase' do
+      it 'sets date range so that the season is open for application submissions' do
+        phase = :fake_application_phase
+        Timecop.travel(Season.current.applications_close_at - 1.month) do
+          Season::PhaseSwitcher.destined(phase)
+          expect(subject).to be_application_period
+        end
       end
     end
-  end
 
-  describe '#fake_coding_phase' do
-    it 'timeshifts the coding phase to today' do
-      Timecop.travel(Season.current.ends_at - 1.week) do
-        Season::PhaseSwitcher.fake_coding_phase
-        expect(subject).to be_started
+    context 'when directed to the #fake_coding_phase' do
+      it 'sets dates so that the summer of code is currently happening' do
+        phase = :fake_coding_phase
+        Timecop.travel(Season.current.ends_at - 1.week) do
+          Season::PhaseSwitcher.destined(phase)
+          expect(subject).to be_started
+        end
       end
     end
-  end
 
-  describe '#fake_proposals_phase' do
-    it 'timeshifts the proposal period to today' do
-      Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
-        fake_time = Time.now #as returned by Timecop
-        Season::PhaseSwitcher.fake_proposals_phase
-        expect(subject.project_proposals_open_at).to be < fake_time
-        expect(subject.project_proposals_close_at).to be > fake_time
+    context 'when directed to the #fake_proposals_phase' do
+      it 'set dates so that season is open for project proposals' do
+        phase = :fake_proposals_phase
+        Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
+          fake_time = Time.now #as returned by Timecop
+          Season::PhaseSwitcher.destined(phase)
+          expect(subject.project_proposals_open_at).to be < fake_time
+          expect(subject.project_proposals_close_at).to be > fake_time
+        end
       end
     end
   end
