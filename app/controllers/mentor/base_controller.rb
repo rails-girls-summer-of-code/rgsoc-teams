302fb680c368a64234d754073c50d1d1b53ebcab
Max Mulatz <klappradla@gmail.com>
2017-01-19 08:34:52 +0100

    Remove commented out code from mentor base controller

--- app/controllers/mentor/base_controller.rb
+++ app/controllers/mentor/base_controller.rb
@@ -1,9 +1,4 @@
 class Mentor::BaseController < ApplicationController
   before_action :authenticate_user!
   before_action -> { require_role 'mentor' }
-  # before_action :set_breadcrumbs
-
-  # def set_breadcrumbs
-  #   @breadcrumbs = [ ['Rating', :rating] ]
-  # end
 end
