e40237e41f58d110494ad2a5246c42a5d89af5f2
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-02-05 20:56:09 +0530

    chore: update email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "confirm@railsgirlssummerofcode.org"
+  config.mailer_sender = "contact@rgsoc.org"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
