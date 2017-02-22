edc8cd2f5bc0f37878cfa3f2979d2c9a01d14cab
Michael Emhofer <michael@emhofer.net>
2017-02-22 01:25:21 +0100

    fix an error thrown when an unconfirmed user logs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,4 +85,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # we are using devise with OmniAuth without other authentications, so we need to define this ourselfes
+  # https://github.com/plataformatec/devise/wiki/OmniAuth%3A-Overview#using-omniauth-without-other-authentications
+  def new_session_path(scope)
+    new_user_session_path
+  end
+
 end
