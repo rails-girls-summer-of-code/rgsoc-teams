21430ce91f2a6bb2010d39aae37610cfda6a9027
alicetragedy <girl@alicetragedy.org>
2017-03-23 15:55:35 +0100

    add less than 40h a week flag to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -79,7 +79,8 @@ class Application < ActiveRecord::Base
           :male_gender,
           :zero_community,
           :age_below_18,
-          :less_than_two_coaches]
+          :less_than_two_coaches,
+          :less_than_40_hours_a_week]
 
   FIELDS_REMOVED_IN_BLIND = [:student0_name,
                              :student1_name,
