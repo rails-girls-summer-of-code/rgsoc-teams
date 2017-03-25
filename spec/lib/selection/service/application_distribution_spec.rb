ad8bfb032aaf7dd724fa6cec4d88a683797ce628
beanieboi <beanie@benle.de>
2017-03-25 16:30:58 +0100

    add spec to make sure we assign 3 reviewers

--- spec/lib/selection/service/application_distribution_spec.rb
+++ spec/lib/selection/service/application_distribution_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 require 'selection/service/application_distribution'
 
 describe Selection::Service::ApplicationDistribution do
-  let(:reviewer) { FactoryGirl.create(:reviewer) }
-  let(:project) { FactoryGirl.create(:project, :in_current_season, :accepted, submitter: reviewer) }
+  let!(:reviewers) { FactoryGirl.create_list(:reviewer, 4) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project, :in_current_season, :accepted, submitter: user) }
   let(:applications) { FactoryGirl.create_list(:application, 10, :in_current_season, :for_project, project1: project) }
 
-  it 'distributes the application randomly to reviewers' do
+  before do
     Todo.destroy_all
+  end
 
+  it 'distributes the application randomly to reviewers' do
     applications.shuffle.each do |application|
       Selection::Service::ApplicationDistribution.new(application: application).distribute
     end
@@ -24,5 +27,15 @@ describe Selection::Service::ApplicationDistribution do
     second_distribution = Todo.all.map {|t| [t.user_id, t.application_id] }.flatten
 
     expect(first_distribution).not_to eq(second_distribution)
+  end
+
+  it 'assignes at least 3 reviewers' do
+    applications.shuffle.each do |application|
+      Selection::Service::ApplicationDistribution.new(application: application).distribute
+    end
+
+    applications.each do |application|
+      expect(Todo.where(application: application).count).to eq(3)
+    end
   end
 end
