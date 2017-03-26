f7d6bb9e7493d10cf4e0776214a4cab492547e56
beanieboi <beanie@benle.de>
2017-03-26 07:09:13 -0400

    update schema to reflect migration state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324095215) do
+ActiveRecord::Schema.define(version: 20170324225335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
