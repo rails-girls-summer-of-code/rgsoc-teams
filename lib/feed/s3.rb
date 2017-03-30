1961ea36d1d98e4057285b1a231f3facb3a48455
ben <beanie@benle.de>
2017-03-29 20:47:44 -0400

    Update AWS-SDK

--- lib/feed/s3.rb
+++ lib/feed/s3.rb
@@ -2,30 +2,32 @@ class Feed
   class S3
     class << self
       def setup(options)
-        AWS.config(access_key_id: options[:access_key_id], secret_access_key: options[:secret_access_key])
+        Aws.config.update(
+          access_key_id: options[:access_key_id], secret_access_key: options[:secret_access_key]
+        )
       end
     end
 
     attr_reader :s3, :url, :logger
 
     def initialize(url, options = {})
-      @s3 = AWS::S3.new
+      @s3 = Aws::S3::Resource.new(stub_responses: Rails.env.test?)
       @url = url
       @logger = options[:logger] || Logger.new(STDOUT)
     end
 
     def store(data, content_type)
-      object.write(data, content_type: content_type, acl: :public_read)
+      object.put(body: data, content_type: content_type, acl: :public_read)
     rescue => e
       logger.error "Could not store to S3: #{e.message}"
     end
 
     def object
-      @object ||= bucket.objects[path]
+      @object ||= bucket.object(path)
     end
 
     def bucket
-      @bucket ||= s3.buckets['rgsoc']
+      @bucket ||= s3.bucket('rgsoc')
     end
 
     def path
