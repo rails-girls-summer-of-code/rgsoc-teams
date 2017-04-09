a596f534b1d48b34a3c52c46ae180572dc89cd22
Max Mulatz <klappradla@gmail.com>
2017-04-09 13:13:14 +0200

    Set vary header for activities index
    This prevents Chrome from responding with the wrong format when using
    the back button. Background: Chrome's cache does not distinguish between
    the formats otherwise.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   before_action :normalize_params, only: :index
   before_action :cors_preflight, only: :index
+  before_action :set_vary_header, only: :index
   after_action  :cors_set_headers, only: :index
 
   before_action :set_activities
@@ -10,33 +11,36 @@ class ActivitiesController < ApplicationController
     respond_to do |format|
       format.html
       format.json { render json: @activities }
-      format.atom { render :layout => false  }
+      format.atom { render layout: false  }
     end
   end
 
   private
 
-    def set_activities
-      @activities = Activity.includes(:team).with_kind(whitelisted_kind).ordered.page(params[:page])
-      @activities = @activities.by_team(params[:team_id]) if params[:team_id].present?
-    end
+  def set_vary_header
+    response.headers['Vary'] = 'Accept'
+  end
 
-    def teams
-      Team.in_current_season.selected.order(:name)
-    end
-    helper_method :teams
+  def set_activities
+    @activities = Activity.includes(:team).with_kind(whitelisted_kind).ordered.page(params[:page])
+    @activities = @activities.by_team(params[:team_id]) if params[:team_id].present?
+  end
 
-    def normalize_params
-      params[:kind] = 'all' if params[:kind].blank?
-    end
+  def teams
+    Team.in_current_season.selected.order(:name)
+  end
+  helper_method :teams
 
-    def whitelisted_kind
-      @whitelisted_kind ||= Array(params[:kind]).detect { |kind| public_activities.include? kind } || public_activities
-    end
+  def normalize_params
+    params[:kind] = 'all' if params[:kind].blank?
+  end
 
-    def public_activities
-      @public_activities ||= %w(feed_entry status_update)
-    end
-    helper_method :public_activities
+  def whitelisted_kind
+    @whitelisted_kind ||= Array(params[:kind]).detect { |kind| public_activities.include? kind } || public_activities
+  end
+
+  def public_activities
+    @public_activities ||= %w(feed_entry status_update)
+  end
+  helper_method :public_activities
 end
-
