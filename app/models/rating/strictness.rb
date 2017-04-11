beb86cd79a36aaf1ca38c70f948cea7809e5a380
Carsten Zimmermann <cz@aegisnet.de>
2017-04-11 21:16:08 +0200

    Memoization

--- app/models/rating/strictness.rb
+++ app/models/rating/strictness.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 class Rating::Strictness
-  class << self
 
-    def ratings
-      Rating
-        .joins('JOIN applications ON ratings.rateable_id = applications.id')
-        .where(rateable_type: 'Application')
-        .where(season_id: Season.current.id)
-    end
-
-    def reviewer_ids
-      ratings.pluck(:user_id).uniq
-    end
-
+  def ratings
+    @ratings ||= Rating
+                  .joins('JOIN applications ON ratings.rateable_id = applications.id')
+                  .where(rateable_type: 'Application')
+                  .where(season_id: Season.current.id)
   end
+
+  def reviewer_ids
+    @reviewer_ids ||= ratings.map(&:user_id).uniq
+  end
+
 end
