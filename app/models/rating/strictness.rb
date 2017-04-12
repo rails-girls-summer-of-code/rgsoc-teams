0974c30cfa6b65deb156774d532723443e6d22a3
Carsten Zimmermann <cz@aegisnet.de>
2017-04-12 12:33:50 +0200

    Calculate adjusted points for each application

--- app/models/rating/strictness.rb
+++ app/models/rating/strictness.rb
@@ -8,12 +8,28 @@ class Rating::Strictness
                   .where('applications.season_id' => Season.current.id)
   end
 
+  def application_ids
+    @application_ids ||= ratings.map(&:rateable_id)
+  end
+
+  def applications
+    @applications ||= Application.includes(:ratings).where(id: application_ids)
+  end
+
   def reviewer_ids
     @reviewer_ids ||= ratings.map(&:user_id).uniq
   end
 
   def average_points_per_reviewer
     @average_points_per_reviewer ||= ratings.sum(&:points) / reviewer_ids.size.to_f
+  end
+
+  def adjusted_points_for_applications
+    applications.each_with_object({}) do |application, map|
+      map[application.id] = application.ratings.sum do |rating|
+        rating.points * strictness_per_reviewer[rating.user_id]
+      end.to_f / application.ratings.count.to_f
+    end
   end
 
   def strictness_per_reviewer
