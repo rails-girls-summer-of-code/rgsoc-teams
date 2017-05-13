021a433ee24bcbde6703dc0175c0f3070ed713ce
F3PiX <f3pixnl@gmail.com>
2017-05-13 21:23:40 +0200

    Better specs 2: only check input and output

--- spec/models/season/phase_switcher_spec.rb
+++ spec/models/season/phase_switcher_spec.rb
@@ -7,63 +7,36 @@ describe Season::PhaseSwitcher do
   end
 
   context 'when switching phases' do
-    subject { Season.current }
+    season = Season.current
 
-    describe 'destined' do
-      xit 'sends the params to a phase setting method' do
+    context 'when the input is valid' do
+      it 'gets called on the season object' do
         phase = :fake_application_phase
-        expect(subject).to receive(Season::PhaseSwitcher.destined(phase))
+        described_class.destined(phase)
+        season.reload
+        expect(season).to be_application_period
       end
 
-      context 'when directed to the #fake_application_phase' do
-        it 'sets date range so that the season is open for application submissions' do
-          phase = :fake_application_phase
-          Timecop.travel(Season.current.applications_close_at - 1.month) do
-            Season::PhaseSwitcher.destined(phase)
-            expect(subject).to_not be_nil
-            expect(subject).to be_application_period
-          end
-        end
+      it 'changes also when real time happens to be within application_period' do
+        phase = :fake_proposals_phase
+        described_class.destined(phase)
+        season.reload
+        expect(season).not_to be_application_period
       end
+    end
 
-      context 'when directed to the #fake_coding_phase' do
-        it 'sets dates so that the summer of code is currently happening' do
-          phase = :fake_coding_phase
-          Timecop.travel(Season.current.ends_at - 1.week) do
-            Season::PhaseSwitcher.destined(phase)
-            expect(subject).to be_started
-          end
-        end
+    context 'when the input is malicious' do
+      phase = :bad_intentions
+
+      it 'raises an error when it receives a non-whitelisted phase' do
+        expect {
+          described_class.destined(phase)
+        }.to raise_error(ArgumentError)
       end
-
-      context 'when directed to the #fake_proposals_phase' do
-        it 'set dates so that season is open for project proposals' do
-          phase = :fake_proposals_phase
-          Timecop.travel(Season.current.project_proposals_close_at - 2.weeks) do
-            fake_time = Time.now #as returned by Timecop
-            Season::PhaseSwitcher.destined(phase)
-            expect(subject.project_proposals_open_at).to be < fake_time
-            expect(subject.project_proposals_close_at).to be > fake_time
-          end
-        end
-      end
-
-      it 'addresses the requested phase' do
-        another_phase = :fake_proposals_phase
-        Season::PhaseSwitcher.destined(another_phase)
-        Season.current
-        expect(subject).to_not be_application_period
-      end
-
-      context 'it does not accept malicious input' do
-        phase = :bad_intentions
-
-        it 'raises an error when it receives a non-whitelisted phase' do
-          expect { Season::PhaseSwitcher.destined(phase) }.to raise_error(ArgumentError)
-        end
-        it 'does not change the subject' do
-          expect { Season::PhaseSwitcher.destined(phase) rescue ArgumentError }.not_to change { subject.reload }
-        end
+      it 'does not change the season dates' do
+        expect {
+          described_class.destined(phase) rescue ArgumentError
+        }.not_to change { Season.current.reload }
       end
     end
   end
