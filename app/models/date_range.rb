760d4c4815bfea5b87af49e4ca54249966ce78bc
F3PiX <f3pixnl@gmail.com>
2017-05-22 17:26:40 +0200

    Declutter

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -15,14 +15,11 @@ class DateRange
     return last_day if @start_date == @end_date
     
     if (@start_date.month == @end_date.month) && (@start_date.year == @end_date.year)
-      # return 01 - 02 Jun 2017
-      first_day = @start_date.strftime("%d")
-      # last_day = @end_date.strftime("%d %b %Y")
+      first_day = @start_date.strftime("%d") # return 01 - 02 Jun 2017
     elsif @start_date.year == @end_date.year
-      # return 31 May - 1 Jun 2017
-      first_day = @start_date.strftime("%d %b")
+      first_day = @start_date.strftime("%d %b") # return 31 May - 1 Jun 2017
     else
-      first_day = @start_date.strftime("%d %b %Y")
+      first_day = @start_date.strftime("%d %b %Y") # return both dates DD MM YYYY
     end
     first_day << ' - ' << last_day
   end
