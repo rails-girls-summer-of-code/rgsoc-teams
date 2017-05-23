03dfe60d763b57ef79bcb10a25834e1c54a717d9
F3PiX <f3pixnl@gmail.com>
2017-05-23 14:12:13 +0200

    Refactor #compact method for DateRange

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -9,18 +9,22 @@ class DateRange
     @start_date = start_date
     @end_date = end_date
   end
-
+  
+  # Accepts dates
+  # Returns a string with one date or a formatted range:
+  # 12 - 13 Jul 2017 or 30 Jul - 01 Aug 2017 or
+  # 31 Dec 2017 - 05 Jan 2018 or 01 Dec 2017 - 12 Dec 2018
   def compact
-    last_day = @end_date.strftime("%d %b %Y")
+    elements = "%d %b %Y" # 1 Jul 2017
+    last_day = @end_date.strftime(elements)
     return last_day if @start_date == @end_date
     
-    if (@start_date.month == @end_date.month) && (@start_date.year == @end_date.year)
-      first_day = @start_date.strftime("%d") # return 01 - 02 Jun 2017
-    elsif @start_date.year == @end_date.year
-      first_day = @start_date.strftime("%d %b") # return 31 May - 1 Jun 2017
-    else
-      first_day = @start_date.strftime("%d %b %Y") # return both dates DD MM YYYY
-    end
+    same_years = @start_date.year == @end_date.year
+    same_months = (@start_date.month == @end_date.month) && same_years
+    elements = elements.gsub(' %Y', '') if same_years
+    elements = elements.gsub(' %b', '') if same_months
+    
+    first_day = @start_date.strftime(elements)
     first_day << ' - ' << last_day
   end
 end
