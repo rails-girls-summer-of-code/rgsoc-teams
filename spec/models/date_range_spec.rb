a0d50fab87039c66d8240534ae6ceec1b3eb377f
F3PiX <f3pixnl@gmail.com>
2017-05-23 14:15:44 +0200

    Add specs for dates with same months in different years
    And add 'when' to context

--- spec/models/date_range_spec.rb
+++ spec/models/date_range_spec.rb
@@ -20,10 +20,10 @@ RSpec.describe DateRange do
 
     context 'depending on date range' do
       it 'returns one day range' do
-        expect(subject.compact).to eq subject.end_date.to_date.strftime('%d %b %Y')
+        expect(subject.compact).to eq subject.end_date.to_date.strftime('%-d %b %Y')
       end
       
-      context 'multiple dates in same month' do
+      context ' when multiple dates in same month' do
         subject { described_class.new(Date.parse('2015-12-30'), Date.parse('2015-12-31')) }
         
         it 'returns compact days' do
@@ -31,19 +31,27 @@ RSpec.describe DateRange do
         end
       end
       
-      context 'multiple dates in different months' do
+      context 'when multiple dates in different months' do
         subject { described_class.new(Date.parse('2015-11-30'), Date.parse('2015-12-01')) }
       
         it 'returns day-month and the year' do
-          expect(subject.compact).to eq ('30 Nov - 01 Dec 2015' )
+          expect(subject.compact).to eq ('30 Nov - 1 Dec 2015' )
         end
       end
       
-      context 'multiple dates around New Year' do
+      context 'when multiple dates around New Year' do
         subject { described_class.new(Date.parse('2015-12-31'), Date.parse('2016-01-01')) }
         
         it 'returns full dates' do
-          expect(subject.compact).to eq ('31 Dec 2015 - 01 Jan 2016')
+          expect(subject.compact).to eq ('31 Dec 2015 - 1 Jan 2016')
+        end
+      end
+      
+      context 'when same months, different years' do
+        subject { described_class.new(Date.parse('2015-12-01'), Date.parse('2016-12-31')) }
+        
+        it 'returns full dates' do
+          expect(subject.compact).to eq ('1 Dec 2015 - 31 Dec 2016')
         end
       end
     end
