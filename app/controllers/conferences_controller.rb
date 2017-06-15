d072b812b5c2be68d25ca03121e49fce164496e4
F3PiX <f3pixnl@gmail.com>
2017-06-15 20:59:57 +0200

    Uniform formatting

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -9,20 +9,21 @@ class ConferencesController < ApplicationController
   end
 
   private
-    def conferences
-      @conferences ||= Conference.ordered(sort_params).in_current_season
-    end
-    helper_method :conferences
+ 
+  def conferences
+    @conferences ||= Conference.ordered(sort_params).in_current_season
+  end
+  helper_method :conferences
 
-    def conference
-       @conference ||= Conference.find(params[:id])
-    end
-    helper_method :conference
+  def conference
+     @conference ||= Conference.find(params[:id])
+  end
+  helper_method :conference
 
-    def sort_params
-      {
-        order: %w(name location starts_on round).include?(params[:sort]) ? params[:sort] : nil,
-        direction: %w(asc desc).include?(params[:direction]) ? params[:direction] : nil
-      }
-    end
+  def sort_params
+    {
+      order: %w(name location starts_on round).include?(params[:sort]) ? params[:sort] : nil,
+      direction: %w(asc desc).include?(params[:direction]) ? params[:direction] : nil
+    }
+  end
 end
