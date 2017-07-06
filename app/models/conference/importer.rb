7e6ffd570c6ee78c0b3027f334c00e56b16e0ad6
F3PiX <f3pixnl@gmail.com>
2017-07-06 14:02:18 +0200

    Improvements in Importer
    - Add explanation of gid ('Google ID')
    - Change custom logger into Rails (with tag 'Importer')
    - Solve class ivar for season id.

--- app/models/conference/importer.rb
+++ app/models/conference/importer.rb
@@ -4,12 +4,13 @@ class Conference::Importer
   ## This importer is depending on agreed-upon input format.
   ## Input file:
   # - Dates should be formatted with dd mm yyyy
-  # - UID should be unique and formatted with 'current season' + id: 2017001
+  # - UID should be unique and formatted with season + id: 2017001
   # - The headers should not be changed
   # Output:
-  # Conferences will be updated or created
-  # Conferences deleted in the input file, are not removed from the table
-  # Import errors are logged in a separate 'imports_logger' file
+  # - Conferences will be updated or created
+  # - Conferences deleted in the input file, are not removed from the table
+  # - Import errors are logged in a Rails Logger
+  # - UID is mapped to 'gid' ('google-id'), and has the format: 2017001
   
   class << self
     def call_with(file)
@@ -26,11 +27,11 @@ class Conference::Importer
         started: "Started importing file #{file.original_filename}",
         finished: "Finished updating/creating #{count_conferences_in(file)} conferences"
       }
-      imports_logger.info "\n***" + notices[arg] + "***"
+      logger.tagged("Importer") { logger.info "\n***" + notices[arg] + "***" }
     end
    
-    def imports_logger
-      Logger.new("#{Rails.root}/log/imports_logger.log")
+    def logger
+      Rails.logger
     end
     
     def check_valid(file)
@@ -45,7 +46,6 @@ class Conference::Importer
       CSV.foreach(file.path, headers: true, col_sep: ';' ) do |row|
         begin
           conference = Conference.find_or_initialize_by(gid: row['UID'])
-          # TODO check: when extra columns are added in the Google Doc, what happens?
           conference_hash = {
             gid: row['UID'].to_i,
             name: row['Name'],
@@ -57,17 +57,17 @@ class Conference::Importer
             url: row['Website'],
             notes: row['Notes'],
           }
-          fetch_season_id(row['UID'])
-          conference.update!(conference_hash.merge(season_id: @season_id))
+          conference.update!(conference_hash.merge(season_id: fetch_season_id(row['UID'])))
         rescue => e
-          imports_logger.error "Error in #{row['UID']}: #{e.message}"
+        logger.tagged("Importer")  { logger.error "Error in #{row['UID']}: #{e.message}" }
         end
       end
     end
     
     def fetch_season_id(uid)
+      # uid format : 2017001
       year = uid.to_s[0,4]
-      @season_id = Season.find_by(name: year).id
+      Season.find_by(name: year).id
     end
   end
 end
