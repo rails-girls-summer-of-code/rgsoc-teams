021bf416b12e848788605a86bc62c96c1b36f525
F3PiX <f3pixnl@gmail.com>
2017-07-06 16:20:47 +0200

    Remove memo about logger
    the custom logger is gone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  # MEMO: check if imports_logger gives output if this is not set?
-  # config.log_level = :info
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
