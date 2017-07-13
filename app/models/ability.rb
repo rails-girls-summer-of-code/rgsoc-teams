9f0e3f0b9584ca99f576760756bf2d261ebcf2a7
juuh42dias <juuh42dias@gmail.com>
2017-07-13 11:40:33 -0300

    refactor method - ability to crud attendance

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -50,7 +50,7 @@ class Ability
     end
 
     can :crud, Attendance do |attendance|
-      user.admin? || Role.where(team_id: attendance.team.id, user_id: user.id, name: 'student').present?
+      user.admin? || (attendance.team.students.include? user)
     end
 
     can :read, Mailing do |mailing|
