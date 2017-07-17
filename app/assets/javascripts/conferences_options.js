e34f5b152ee4afaba56fc2229a1dd223cb6b5302
juuh42dias <juuh42dias@gmail.com>
2017-07-17 13:48:08 -0300

    fix js using jshint suggestions

--- app/assets/javascripts/conferences_options.js
+++ app/assets/javascripts/conferences_options.js
@@ -1,20 +1,20 @@
 $(function() {
   $('#team_attendances_attributes_0_conference_id').on("change", function() {
       $("select option").attr("disabled",false);
-      DisableOptions();
+      disableOptions();
     });
 
-  function DisableOptions(){
+  function disableOptions(){
     $("select option").filter(function(){
       var bSuccess=false;
       var selectedEl=$(this);
       $("select option:selected").each(function(){
-        if($(this).val()==selectedEl.val()){
+        if($(this).val()===selectedEl.val()){
           bSuccess=true;
           return false;
         }
       });
         return bSuccess;
       }).css("display","none");
-    };
+    }
 });
