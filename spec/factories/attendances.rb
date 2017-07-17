4fb8a40929427f0e17cc03d6c5b08e43489ef832
branquinhoaa <amanda.alves15@gmail.com>
2017-07-17 16:19:44 -0300

    fix identation for attendance factory

--- spec/factories/attendances.rb
+++ spec/factories/attendances.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :attendance do
-	  team { FactoryGirl.create(:team, :in_current_season) }
-	  conference { FactoryGirl.create(:conference, :in_current_season) }
+    team { FactoryGirl.create(:team, :in_current_season) }
+    conference { FactoryGirl.create(:conference, :in_current_season) }
 
     trait :student_attendance do
       after(:create) do |attendance|
