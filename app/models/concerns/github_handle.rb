9b9a3872bcc8cf1d3f2578c243ee6fc24e39aa29
Juliana Dias <juuh42dias@gmail.com>
2017-07-19 12:02:18 -0300

    Adding if condition because that return true or false

--- app/models/concerns/github_handle.rb
+++ app/models/concerns/github_handle.rb
@@ -6,8 +6,10 @@ module GithubHandle
   end
 
   def github_handle=(github_handle)
-    self.user = github_handle.present? && User.where("github_handle ILIKE ?", github_handle).first_or_initialize(github_handle: github_handle)
-    user.github_handle = github_handle
-    user.github_import = true
+    if github_handle.present?
+      self.user = User.where("github_handle ILIKE ?", github_handle).first_or_initialize(github_handle: github_handle)
+      user.github_handle = github_handle
+      user.github_import = true
+    end
   end
 end
