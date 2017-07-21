e8fd14735f09d233a70c34f554aba436af696349
juuh42dias <juuh42dias@gmail.com>
2017-07-21 09:41:37 -0300

    remove github handle module

--- app/models/conference_preference.rb
+++ app/models/conference_preference.rb
@@ -1,5 +1,4 @@
 class ConferencePreference < ActiveRecord::Base
-  include GithubHandle
 
   belongs_to :team, inverse_of: :conference_preferences
   belongs_to :conference, inverse_of: :conference_preferences
