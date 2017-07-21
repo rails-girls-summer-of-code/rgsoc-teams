aab74b24e9fbc2f3df2608457d1ae9d20eb119aa
F3PiX <f3pixnl@gmail.com>
2017-07-21 13:00:53 +0200

    Solved mystery with failing shared_context
    Restored 'build_stubbed user' instead of 'build user' in the shared context file :bug:

--- spec/controllers/mailings_controller_spec.rb
+++ spec/controllers/mailings_controller_spec.rb
@@ -3,23 +3,12 @@ require 'spec_helper'
 describe MailingsController do
   render_views
 
-  let!(:mailing) { FactoryGirl.create(:mailing) }
-  let!(:user) { FactoryGirl.create(:user) }
+  let!(:mailing) { create(:mailing) }
+  let!(:user) { create(:user) }
 
   describe 'GET index' do
     context 'with user logged in' do
-
-      # I wanted to use the shared_context 'with user logged in'
-      # If I do that, instead of the `before { sign_in user }`,
-      # it throws the following error:
-      # " MailingsController GET index with user logged in renders the index
-      # " Failure/Error: expect(response).to render_template 'index'
-      # " expecting <"index"> but was a redirect to <http://test.host/>
-        #" ./spec/controllers/mailings_controller_spec.rb:20:in `block (4 levels) in <top (required)>'
-      #
-      #include_context 'with user logged in' # => red
-
-      before { sign_in user }                 # => green
+      include_context 'with user logged in'
 
       it 'renders the index' do
         get :index
--- spec/support/shared_contexts/with_user_logged_in.rb
+++ spec/support/shared_contexts/with_user_logged_in.rb
@@ -1,5 +1,5 @@
 RSpec.shared_context 'with user logged in' do
-  let(:current_user) { build(:user) }
+  let(:current_user) { build_stubbed(:user) }
 
   before do
     allow(controller).to receive(:current_user).and_return(current_user)
