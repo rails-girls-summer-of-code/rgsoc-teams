8e49daae69dbe407ce17a05569373a8d725a55fc
F3PiX <f3pixnl@gmail.com>
2017-07-26 10:03:17 +0200

    Finetuning loading and authorizing of resource :zap:

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,17 +1,15 @@
 class MailingsController < ApplicationController
 
-  load_and_authorize_resource
+  load_and_authorize_resource except: :index
 
   def index
     @mailings = Mailing.order('id DESC').page(params[:page])
     authorize! :read, :mailing
   end
 
-  def show
-    @mailing = Mailing.find(params[:id])
-  end
-
+  # These actions are here to enable the cancancan 'not authorised' notice
+  # instead of a Rails exception.
+  def show; end
   def new; end
   def edit; end
-
 end
