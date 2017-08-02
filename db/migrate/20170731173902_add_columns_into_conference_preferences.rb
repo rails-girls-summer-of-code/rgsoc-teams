e73446bfbcb7fba25b1307ba7662b84850a4254d
Juliana Dias <juuh42dias@gmail.com>
2017-08-02 08:49:29 -0300

    Fix var value name

--- db/migrate/20170731173902_add_columns_into_conference_preferences.rb
+++ db/migrate/20170731173902_add_columns_into_conference_preferences.rb
@@ -1,7 +1,7 @@
 class AddColumnsIntoConferencePreferences < ActiveRecord::Migration[5.1]
   def self.up
     add_column :conference_preferences, :comment, :text
-    add_column :conference_preferences, :lightning_talk, :boolean, default: f
+    add_column :conference_preferences, :lightning_talk, :boolean, default: false
   end
 
   def self.down
