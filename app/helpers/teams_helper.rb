2c584643efb99a4a91259903eb19013d904ffc47
Juliana Dias <juuh42dias@gmail.com>
2017-08-07 09:21:00 -0300

    Changes in helper method conference_exists_for?

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,11 @@
 module TeamsHelper
 
-  def conference_exists_for(team)
-    team&.conference_preference&.persisted? && team&.conference_preference&.first_conference_id.present? || team&.conference_preference&.second_conference_id.present?
+  def conference_exists_for?(team)
+    conference_preference = team.conference_preference
+    return false unless conference_preference.persisted?
+    [
+      conference_preference.first_conference_id,
+      conference_preference.second_conference_id
+    ].any?
   end
 end
