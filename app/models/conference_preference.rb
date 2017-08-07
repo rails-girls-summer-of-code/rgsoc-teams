5f076d2373083d84bf98093e373ece726bf0684b
Juliana Dias <juuh42dias@gmail.com>
2017-08-07 12:37:57 -0300

    Change name validation on conference_preference and add condition into teams helper

--- app/models/conference_preference.rb
+++ app/models/conference_preference.rb
@@ -1,6 +1,5 @@
 class ConferencePreference < ActiveRecord::Base
-  validates :terms_accepted?, acceptance: true
-
+  validates :terms, acceptance: true
   attr_accessor :terms_of_ticket, :terms_of_travel
 
   belongs_to :team, inverse_of: :conference_preference
@@ -9,13 +8,11 @@ class ConferencePreference < ActiveRecord::Base
 
   private
 
-    def terms_accepted?
+    def terms
       conference = first_conference_id.present? || second_conference_id.present?
       terms_checkbox = terms_of_ticket == '1' && terms_of_travel == '1'
       first_case = conference.present? && terms_checkbox.present?
       second_case = conference.blank? && terms_checkbox.blank?
-      errors.add(:team, "If you choose any conference you must accept the terms") if conference && !terms_checkbox
-      errors.add(:team, "If you accept terms you must choose any conference") if !conference && terms_checkbox
       first_case || second_case
     end
 end
