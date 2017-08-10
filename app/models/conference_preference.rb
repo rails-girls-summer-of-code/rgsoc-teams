99b64a9310d731403c94d6d3c8d43862f003693d
Juliana Dias <juuh42dias@gmail.com>
2017-08-10 18:20:45 -0300

    Add method for change checkbox status when conference is none

--- app/models/conference_preference.rb
+++ app/models/conference_preference.rb
@@ -1,6 +1,7 @@
 class ConferencePreference < ActiveRecord::Base
   validates :terms_of_ticket, inclusion: { in: [true] }, if: :conference_exists?
   validates :terms_of_travel, inclusion: { in: [true] }, if: :conference_exists?
+  before_save :change_status_terms, unless: :conference_exists?
 
   belongs_to :team, inverse_of: :conference_preference
   belongs_to :first_conference, class_name: 'Conference'
@@ -9,5 +10,10 @@ class ConferencePreference < ActiveRecord::Base
   private
     def conference_exists?
       first_conference_id.present? || second_conference_id.present?
-    end     
+    end
+
+    def change_status_terms
+      self.terms_of_travel = false
+      self.terms_of_ticket = false
+    end
 end
