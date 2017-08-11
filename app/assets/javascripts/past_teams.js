ecd4a2e78771150ae222538899c1046d7d156218
branquinhoaa <amanda.alves15@gmail.com>
2017-08-11 12:03:00 -0300

    include file js for get past teams

---
+++ app/assets/javascripts/past_teams.js
@@ -0,0 +1,27 @@
+//= require 'fullcalendar'
+
+$(function() {
+  var selectYear = $('#calendarContainer');
+  if(container.length > 0) {
+    $.ajax({
+      url: '/calendar/events',
+      success: function(events){
+        $('#loadingMessage').hide();
+        var result = [];
+        for(var i=0; i < events.length; i++)
+        {
+          var input = events[i], output = {};
+          output.title = input.name;
+          output.start = new Date(input.starts_on);
+          output.end = new Date(input.ends_on);
+          output.allDay = true;
+          output.url = "/conferences/"+input.id;
+          result.push(output);
+        }
+        $('#calendarContainer').fullCalendar({
+          events: result
+        });
+      }
+    });
+  }
+});
