e4ab5fada5a4db5a2331047f2f0c81c9034246f5
branquinhoaa <amanda.alves15@gmail.com>
2017-08-21 10:55:19 -0300

    include new fields - related to conference attendances - into exporter file

--- app/exporters/conference_preferences.rb
+++ app/exporters/conference_preferences.rb
@@ -3,10 +3,23 @@ module Exporters
 
     def current
       preferences = ConferencePreference.current_teams
+      team_max_offer = Team.joins(:conference_attendances).group("teams.id").order("count(teams.id) DESC").first
+      max_offer = team_max_offer.conference_attendances.size
 
-      generate(preferences, 'Team name', 'Team location', 'Project name', 'Conference primary choice', 'Conference secondary choice', 'We would like to give a LT', 'Comments', 'Terms accepted') do |cp|
-        [cp.team&.name, cp.team&.students_location, cp.team&.project_name, cp.first_conference&.name, cp.second_conference&.name, cp.lightning_talk, cp.comment, cp.terms_accepted?]
+      header = 'Team name', 'Team location', 'Project name', 'Conference primary choice', 'Conference secondary choice', 'We would like to give a LT', 'Comments', 'Terms accepted'
+      max_offer.times do |n|
+        header << "Conference Offer #{n+1}"
+        header << "Conference Offer #{n+1} accepted"
+      end
+
+      generate(preferences, header.join(',')) do |cp|
+          team_preferences = [cp.team&.name, cp.team&.students_location, cp.team&.project_name, cp.first_conference&.name, cp.second_conference&.name, cp.lightning_talk, cp.comment, cp.terms_accepted?]
+          cp.team&.conference_attendances&.each do |ca|
+            team_preferences << ca.conference.name
+            team_preferences << ca.attendance
+          end
+          team_preferences
+        end
       end
     end
   end
-end
