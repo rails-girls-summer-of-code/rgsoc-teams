5a1076a3e80e0068fe758154f8e5e304f03e7db0
branquinhoaa <amanda.alves15@gmail.com>
2017-08-24 13:02:22 -0300

    create a file for conference attendance helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -211,22 +211,4 @@ module ApplicationHelper
   def list_all_timezones
     TIMEZONES
   end
-
-  #conference attendance helper
-
-  def show_attendance(attendance)
-    label_class = case attendance
-                when true
-                  'label-success'
-                when false
-                  'label-warning'
-                end
-    text_label = case attendance
-                when true
-                  "I will attend this conference"
-                when false
-                  "I will not attend this conference"
-                end
-    content_tag :span, text_label, class: "label #{label_class}"
-  end
 end
