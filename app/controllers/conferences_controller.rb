36e2e0d025a6b18b25afb3f2c32e597f8a7acfa9
Juliana Dias <juuh42dias@gmail.com>
2017-08-24 15:19:00 -0300

    Adjust redirect pages

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -15,26 +15,33 @@ class ConferencesController < ApplicationController
   end
 
   def create
-    team = current_user.student_team
     @conference = Conference.new(conference_params)
     @conference.season_id = current_season.id
-    @conference.gid = generate_gid(team)
+    @conference.gid = generate_gid(current_user)
 
     if @conference.save
-      redirect_to edit_team_path(team), notice: 'Conference was successfully created.'
+      redirect
     else
       render action: :new
     end
   end
 
   def redirect
-    redirect_to orga_conferences_path
+    message = 'Conference was successfully created.'
+    case true
+    when current_user.current_student?
+      redirect_to edit_team_path(current_student.current_team), notice: message
+    when current_user.admin?
+      redirect_to orga_conferences_path, notice: message
+    else
+      redirect_to @conference, notice: message
+    end
   end
 
   private
 
-  def generate_gid(team)
-    "#{Season.current.name}-#{Time.now.getutc.to_i}-#{team.id}"
+  def generate_gid(user)
+    "#{Season.current.name}-#{Time.now.getutc.to_i}-#{user.id}"
   end
  
   def conferences
@@ -48,7 +55,7 @@ class ConferencesController < ApplicationController
   end
 
   def confirm_role
-    redirect unless current_user.admin? || current_user.student?
+    redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_user.admin? || current_user.current_student?
   end
 
   def sort_params
