5609898e490e2c0f0d1790a39d4795ca526e1673
branquinhoaa <amanda.alves15@gmail.com>
2017-08-25 08:56:18 -0300

    remove team variable

--- app/controllers/conference_attendances_controller.rb
+++ app/controllers/conference_attendances_controller.rb
@@ -2,10 +2,9 @@ class ConferenceAttendancesController < ApplicationController
 
   def update
     @conference_attendance = ConferenceAttendance.find(params[:id])
-    @team = @conference_attendance.team
 
     if @conference_attendance.update_attributes(attendance_params)
-      redirect_to @team, notice: 'Your attendance was successfully recorded.'
+      redirect_to @conference_attendance.team, notice: 'Your attendance was successfully recorded.'
     else
       render json: @conference_attendance.errors, status: :unprocessable_entity
     end
