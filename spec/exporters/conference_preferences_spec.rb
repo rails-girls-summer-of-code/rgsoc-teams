f3be21605c81f1ca7cc2290f70d724581c0beefa
branquinhoaa <amanda.alves15@gmail.com>
2017-08-25 11:19:09 -0300

    add test for exporter

--- spec/exporters/conference_preferences_spec.rb
+++ spec/exporters/conference_preferences_spec.rb
@@ -3,15 +3,9 @@ require 'spec_helper'
 RSpec.describe Exporters::ConferencePreferences do
 
   describe '#current' do
-    before do
-      team1 = double('team')
-      team2 = double('team')
-      allow(ConferencePreference).to receive(:current_teams).and_return([team1, team2])
-    end
-
-    it 'returns a CSV file with conference preferences data from current teams' do
-      get orga_exports_path
-      expect(described_class.current).to match 'NEWPROJECT'
+    it 'calls the current_teams scope in conference preference' do
+      expect(ConferencePreference).to receive(:current_teams).and_return([])
+      described_class.current
     end
   end
 end
