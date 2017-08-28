81450b3b20777735416bc5c3200638a99dce5e67
juuh42dias <juuh42dias@gmail.com>
2017-08-28 15:51:58 -0300

    remove action index from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,9 @@
 class UsersController < ApplicationController
-  before_action :normalize_params, only: :index
   before_action :set_user, only: [:show, :edit, :update, :destroy, :impersonate]
 
   load_and_authorize_resource except: [:index, :show, :impersonate, :stop_impersonating]
 
   def index
-    @filters = {
-      all:        'All',
-      pair:       'Looking for a pair',
-      coaching:   'Helping as a Coach',
-      mentoring:  'Helping as a Mentor',
-      organizing: 'Helping as an Organizer'
-    }
-    @users = User.ordered(params[:sort],params[:direction])
-        .group('users.id').with_all_associations_joined
-    @users = @users.with_assigned_roles if Time.now.utc > (current_season.starts_at || Date.new)
-    @users = @users.with_role(params[:role]) if params[:role].present? && params[:role] != 'all'
-    @users = @users.with_interest(params[:interest]) if params[:interest].present? && params[:interest] != 'all'
-    @users = Kaminari.paginate_array(@users.search(params[:search])).page(params[:page])
   end
 
   def show
@@ -122,9 +108,5 @@ class UsersController < ApplicationController
         interested_in: [],
         roles_attributes: [:id, :name, :team_id, :_destroy]
       )
-    end
-
-    def normalize_params
-      params[:role] = 'all' if params[:role].blank?
     end
 end
