62f5932e0d4233e40e9c3b0b88835003c269972a
Maya G <mayu666@gmail.com>
2017-08-30 10:07:52 +0200

    Added a module in the concerns folder to reduce duplicate code in conferences controllers (#842)
    * Added a module in the concerns folder to reduce duplicate code in conferences controllers
    
    * Renamed module, removed concern, refactored index method, added incomplete spec
    
    * Fixed the ordered conferences spec

---
+++ app/controllers/concerns/ordered_conferences.rb
@@ -0,0 +1,14 @@
+module OrderedConferences
+  def index
+    @conferences = Conference.ordered(sort_params).in_current_season
+  end
+
+  private
+
+  def sort_params
+    {
+      order: %w(name gid starts_on city country region).include?(params[:sort]) ? params[:sort] : nil,
+      direction: %w(asc desc).include?(params[:direction]) ? params[:direction] : nil
+    }
+  end
+end
