32dd8af92bad7ddc01187f040e342697c43298c8
branquinhoaa <amanda.alves15@gmail.com>
2017-08-30 12:57:31 -0300

    change test for redirect to user profile after impersonate user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -208,9 +208,9 @@ RSpec.describe UsersController do
       expect(controller.current_user).to eq impersonated_user
     end
 
-    it 'redirects to community#index' do
+    it 'redirects to user#show' do
       post :impersonate, params: { id: impersonated_user.id }
-      expect(response).to redirect_to community_index_path
+      expect(response).to redirect_to user_path(impersonated_user)
       expect(flash[:notice]).to include "Now impersonating #{impersonated_user.name}"
     end
   end
