c021498c554aa8749631665b14186432fbf84413
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-20 10:30:59 -0600

    removes trailing whitespace from navigation spec

--- spec/requests/navigation_spec.rb
+++ spec/requests/navigation_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Navigation', type: :request do
         expect(response.body).to include activities_path
         expect(response.body).to include teams_path
         expect(response.body).to include conferences_path
-        expect(response.body).to include projects_path        
+        expect(response.body).to include projects_path
         expect(response.body).to include users_path
         expect(response.body).to include page_path(:help)
         expect(response.body).to include user_github_omniauth_authorize_path
@@ -193,7 +193,7 @@ RSpec.describe 'Navigation', type: :request do
         expect(response.body).to include apply_path
         expect(response.body).to include teams_path
         expect(response.body).to include users_path
-        expect(response.body).to include projects_path        
+        expect(response.body).to include projects_path
         expect(response.body).to include page_path(:help)
         expect(response.body).to include user_github_omniauth_authorize_path
       end
