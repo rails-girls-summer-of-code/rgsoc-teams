d1844350eabc7838fb529cd7fa4a9be9cd606060
F3PiX <f3pixnl@gmail.com>
2017-12-07 12:47:57 +0100

    Forgot half of the commits :ghost:

--- spec/helpers/application_form_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ApplicationFormHelper, type: :helper do
-  describe '.time_span_array' do
-    it 'returns an array of timespans' do
-      expect(time_span_array.size).to eq(6)
-    end
-  end
-end
