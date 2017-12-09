6ade7c3528ef0d9276ced132d35ec4c77c488a8c
F3PiX <f3pixnl@gmail.com>
2017-12-09 11:54:11 +0100

    Collect all conf helper methods in the application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,6 +76,22 @@ module ApplicationHelper
       link_to("#{conference.name} (#{details})", conference)
     end
   end
+
+  def show_attendance(attendance)
+    label_class = case attendance
+      when true
+        'label-success'
+      when false
+        'label-warning'
+    end
+    text_label = case attendance
+      when true
+        "I will attend this conference"
+      when false
+        "I will not attend this conference"
+    end
+    content_tag :span, text_label, class: "label #{label_class}"
+  end
   # end Conferences
 
   def if_present?(user, *attrs)
