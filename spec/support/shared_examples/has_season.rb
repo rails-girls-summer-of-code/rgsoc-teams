9aaf05b5c07a18636692fa6941ae702bf4361291
Carsten Zimmermann <cz@aegisnet.de>
2018-01-04 14:59:07 +0100

    Add HasSeason.in_season

--- spec/support/shared_examples/has_season.rb
+++ spec/support/shared_examples/has_season.rb
@@ -1,3 +1,16 @@
 RSpec.shared_examples 'HasSeason' do
   it { expect(subject).to belong_to :season }
+
+  describe '.in_season' do
+    it 'returns a relation' do
+      expect(described_class.in_season('1984')).to be_kind_of ActiveRecord::Relation
+    end
+
+    it 'accepts an String as well as any object responding to #name' do
+      sql_string = described_class.in_season('test').to_sql
+      sql_double = described_class.in_season(double(name: 'test')).to_sql
+
+      expect(sql_double).to eql sql_string
+    end
+  end
 end
