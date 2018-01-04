958d090ad17d5edd3bbeee448690e7042060c27a
Carsten Zimmermann <cz@aegisnet.de>
2018-01-04 13:58:48 +0100

    Use data-behaviour attributes

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-    $('#team-sources input[id^=team_sources_attributes]:radio').change(function() {
-        var helpText,
-            helpNode = $($(this).parents()[2]).find('.help-block');
+    $('div[data-behaviour=source-kind-switch] input:radio').change(function() {
+        var helpText = "",
+            helpNode = $(this).parents('div[data-behaviour=source-kind-switch]').find('.help-block');
 
         if (this.value === 'blog') {
             helpText = "<div class='alert alert-warning'>Sources of type <b>Blog</b> will " +
                        "periodically synchronise all its posts. If you want to add a single " +
                        "web page, please select the <b>Page</b> option.</div>";
-        } else {
-            helpText = "";
         }
         helpNode.html(helpText);
     });
