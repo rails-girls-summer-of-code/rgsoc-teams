dd3d4618ae1b666a41a660a57bc278d54f892853
Carsten Zimmermann <cz@aegisnet.de>
2018-01-05 15:43:28 +0100

    Move order to spec_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,8 +51,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
 
-  config.order = 'random'
-
   config.include ActiveJob::TestHelper
   config.include RSpecHtmlMatchers
 
