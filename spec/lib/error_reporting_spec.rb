729bf8bc62dfedc4ccce5900fed905d3b582bebe
Carsten Zimmermann <cz@aegisnet.de>
2018-01-07 18:26:55 +0100

    Create an error reporting facade (#907)
    * Create an error reporting facade
    
    While exception tracker libraries tie into Ruby's/Rails' system
    automatically for uncaught exceptions, using the exception tracker's
    log/system/dashboard for rescued errors requires special API calls.
    
    We still haven't figured out which exception tracker we want to use
    (we've used Sentry, Honeybadger, and Rollbar so far). This simple class
    makes it easy to swap error logging providers for manual error reports.
    
    * Fix typo (if it's a typo)
    
    [ci skip]

---
+++ spec/lib/error_reporting_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'error_reporting'
+
+RSpec.describe ErrorReporting do
+  describe '.call' do
+    let(:raven_double) { double(capture_message: :ok, capture_exception: :ok) }
+
+    before do
+      stub_const('Raven', raven_double)
+    end
+
+    let(:error) { Class.new(Exception).new("Nope.") }
+    let(:string) { "Err, what about … no?" }
+
+    it 'will send descendants of Exception' do
+      described_class.call error
+      expect(raven_double).to have_received(:capture_exception)
+    end
+
+    it 'will send a simple String message' do
+      described_class.call string
+      expect(raven_double).to have_received(:capture_message)
+    end
+  end
+end
