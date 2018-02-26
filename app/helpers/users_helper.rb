50f3d06114b375ac3fc06adb5ed03a95e2565861
Ramón Huidobro <ramon.martin.huidobro@gmail.com>
2018-02-26 19:55:45 +0100

    Show all teams if user is same as current (#950)
    * Show all teams if user is same as current
    
    * Tweak specs

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+module UsersHelper
+
+  def teams_for(user)
+    user == current_user ? user.teams : user.teams.visible
+  end
+end
---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe UsersHelper, type: :helper do
+
+  describe "#teams_for" do
+    let(:user) { create :user }
+    let(:invisible_team) { create :team, invisible: true, kind: nil }
+    let(:visible_team) { create :team, invisible: false, kind: nil }
+    let(:role_name) { 'student' }
+
+    before do
+      create(:role, name: 'student', user: user, team: invisible_team)
+      create(:role, name: 'student', user: user, team: visible_team)
+    end
+
+    context 'user signed in is viewing own teams' do
+      before do
+        allow(self).to receive(:current_user).and_return(user)
+      end
+
+      it 'returns all teams' do
+        expect(teams_for user).to contain_exactly(invisible_team, visible_team)
+      end
+    end
+
+    context 'user not signed in is viewing teams' do
+      before do
+        allow(self).to receive(:current_user).and_return(nil)
+      end
+
+      it 'returns only visible teams' do
+        expect(teams_for user).to contain_exactly(visible_team)
+      end
+    end
+  end
+end
