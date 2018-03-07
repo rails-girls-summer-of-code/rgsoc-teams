b792ea87c3e082454b31234b6b81b059004c03e5
Carsten Zimmermann <cz@aegisnet.de>
2018-03-07 23:37:05 +0100

    Remove redundant keyword that was a redundant keyword

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
 
   belongs_to :submitter, class_name: 'User'
   has_many :project_maintenances, dependent: :nullify
-  has_many :maintainers, through: :project_maintenances, source: :user, class_name: 'User'
+  has_many :maintainers, through: :project_maintenances, source: :user
   has_many :comments, -> { order('created_at DESC') }, as: :commentable, dependent: :destroy
 
   has_many :first_choice_application_drafts,  class_name: 'ApplicationDraft', foreign_key: 'project1_id'
