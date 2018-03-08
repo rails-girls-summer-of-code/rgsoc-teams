f0f94189fd70cccc6c811ca670a59970ba187d2e
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2018-03-08 10:12:42 +0100

    Consolidate flags, remove volunteer flag

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,13 +5,7 @@ class Application < ApplicationRecord
   PROJECT_VISIBILITY_WEIGHT = ENV['PROJECT_VISIBILITY_WEIGHT'] || 2
   COACHING_COMPANY_WEIGHT = ENV['COACHING_COMPANY_WEIGHT'] || 2
   MENTOR_PICK_WEIGHT = ENV['MENTOR_PICK_WEIGHT'] || 2
-  FLAGS = [:remote_team,
-          :volunteering_team,
-          :selected,
-          :male_gender,
-          :zero_community,
-          :age_below_18,
-          :less_than_two_coaches]
+  FLAGS = Selection::Table::FLAGS + [:selected]
 
   belongs_to :application_draft
   belongs_to :team, inverse_of: :applications, counter_cache: true
