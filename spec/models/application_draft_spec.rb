868a002927009828a7c7c9373e1176e732249bd0
Cess <cessmbuguar@gmail.com>
2018-03-12 11:59:24 +0300

    Restore email sending code
    So as to investigate t he bug that find solution

--- spec/models/application_draft_spec.rb
+++ spec/models/application_draft_spec.rb
@@ -329,13 +329,24 @@ RSpec.describe ApplicationDraft, type: :model do
         expect(application).to have_attributes(application_draft: application_draft)
       end
 
-      it 'updates the state to :applied and sends notification email to orga' do
+      it 'updates the state to :applied and sends notification emails to orga and submitters' do
         expect { submit_application }.to change { application_draft.state }.from('draft').to('applied')
 
         submitted_application = application_draft.reload.application
 
         expect(ActionMailer::DeliveryJob).to have_been_enqueued.with(
           'ApplicationFormMailer', 'new_application', 'deliver_now', submitted_application
+        )
+        expect(ActionMailer::DeliveryJob).to have_been_enqueued.with(
+          'ApplicationFormMailer', 'submitted', 'deliver_now',
+          application: submitted_application,
+          student:     students.first
+
+        )
+        expect(ActionMailer::DeliveryJob).to have_been_enqueued.with(
+          'ApplicationFormMailer', 'submitted', 'deliver_now',
+          application: submitted_application,
+          student:     students.second
         )
       end
     end
