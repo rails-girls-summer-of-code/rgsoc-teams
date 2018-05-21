419bdf767d92dacee35285764e459b9d49d027e7
F3PiX <f3pixnl@gmail.com>
2018-05-21 10:54:50 +0200

    Declutter

--- spec/models/ability/team_member_spec.rb
+++ spec/models/ability/team_member_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 require 'cancan/matchers'
 
-# Run this file with
-#   $ rspec spec/models/ability_spec.rb -fd
-# to see the output of specs running inside the shared examples [mdv]
 RSpec.describe Ability, type: :model do
 
   let(:user) { create(:user) }
