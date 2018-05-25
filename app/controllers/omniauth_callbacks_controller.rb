52e956680aa048db5be5c1acd185014a5098c2fe
Maud de Vries <f3pixnl@gmail.com>
2018-05-25 20:36:17 +0200

    Step 1: direct more users to their edit profile page

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.find_or_create_for_github_oauth(request.env['omniauth.auth'])
 
-    if user.just_created? || !user.valid? || user.github_import
+    if user.just_created? || !user.valid? || user.github_import || !user.confirmed? || user.email.nil? || user.github_id.nil?
       sign_in user
       redirect_to edit_user_path(user, welcome: true)
     elsif user.persisted?
@@ -11,7 +11,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user, event: :authentication
     else
       session['devise.github_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url  # this route doesn't go anywhere (we don't use Devise registerable module)
     end
   end
 end
