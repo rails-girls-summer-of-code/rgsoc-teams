edeacd6765da82312767e0b4b44f5a01f32cf892
Maud de Vries <f3pixnl@gmail.com>
2018-05-27 09:41:50 +0200

    Fix typo

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.find_or_create_for_github_oauth(request.env['omniauth.auth'])
-    flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
+    flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'GitHub'
     sign_in_and_redirect user, event: :authentication
   end
 
