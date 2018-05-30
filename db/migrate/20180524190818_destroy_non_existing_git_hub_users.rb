62b49c4e1aa799de910295323123c592b066af5f
Maud de Vries <f3pixnl@gmail.com>
2018-05-30 18:47:18 +0200

    Handle user records without github_id; skip in test env

--- db/migrate/20180524190818_destroy_non_existing_git_hub_users.rb
+++ db/migrate/20180524190818_destroy_non_existing_git_hub_users.rb
@@ -1,6 +1,14 @@
 class DestroyNonExistingGitHubUsers < ActiveRecord::Migration[5.1]
+
   def up
-    unless ENV['RAILS_ENV'] == 'development'
+    return if ENV['RAILS_ENV'] == 'test'
+    if ENV['RAILS_ENV'] == 'development'
+      puts "Updating users with github_id ...."
+      users = User.all.select { |user| user.github_id.nil? }
+      users.each_with_index do |user, index|
+        user.update!(github_id: index + 10)
+      end
+    else
       User.where(github_id: nil).destroy_all.each do |user|
         puts "removed user #{user.id} from database"
       end
