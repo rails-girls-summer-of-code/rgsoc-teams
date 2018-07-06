b016ff2a1846f129c1283065bdd295ddf267ce4d
Max Mulatz <klappradla@gmail.com>
2018-07-07 01:35:58 +0200

    Change window size for headless chrome driver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,16 @@
 require 'capybara-screenshot/rspec'
 require 'selenium/webdriver'
 
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu --window-size=1024,1300] }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
-  config.javascript_driver = :selenium_chrome_headless
+  config.default_driver = :headless_chrome
+  config.javascript_driver = :headless_chrome
 
   config.server_port = 8888 + ENV['TEST_ENV_NUMBER'].to_i
 
