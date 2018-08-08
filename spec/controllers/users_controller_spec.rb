8dde3dae768d8b8e5894a05d97b1cc5b2aab1b7c
Ramon Huidobro <hola@ramonhuidobro.com>
2018-08-08 12:18:16 +0200

    Cleanups

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -85,6 +85,50 @@ RSpec.describe UsersController, type: :controller do
 
     context "its own profile" do
       describe "with valid params" do
+        context "communication opt-in" do
+          let(:now) { Time.new(2002, 10, 31) }
+          before do
+            Timecop.freeze(now)
+          end
+
+          after do
+            Timecop.return
+          end
+
+          shared_examples_for 'tracks opt-in time' do |attribute|
+            it "sets #{attribute} to the current time" do
+              put :update, params: { id: user.to_param, user: { attribute => '1' } }
+              expect(user.reload.send "#{attribute}_at").to eq(now)
+            end
+          end
+
+          it_behaves_like 'tracks opt-in time', :opted_in_newsletter
+          it_behaves_like 'tracks opt-in time', :opted_in_announcements
+          it_behaves_like 'tracks opt-in time', :opted_in_marketing_announcements
+          it_behaves_like 'tracks opt-in time', :opted_in_surveys
+          it_behaves_like 'tracks opt-in time', :opted_in_sponsorships
+          it_behaves_like 'tracks opt-in time', :opted_in_applications_open
+
+          context 'opting out' do
+            shared_examples_for 'tracks opt-out' do |attribute|
+              before do
+                user.update_attribute attribute, Time.new(2002, 10, 31)
+              end
+
+              it "sets #{attribute} to nil" do
+                put :update, params: { id: user.to_param, user: { attribute => '0' } }
+                expect(user.reload.send "#{attribute}_at").to eq(nil)
+              end
+            end
+
+            it_behaves_like 'tracks opt-out', :opted_in_newsletter
+            it_behaves_like 'tracks opt-out', :opted_in_announcements
+            it_behaves_like 'tracks opt-out', :opted_in_marketing_announcements
+            it_behaves_like 'tracks opt-out', :opted_in_surveys
+            it_behaves_like 'tracks opt-out', :opted_in_sponsorships
+            it_behaves_like 'tracks opt-out', :opted_in_applications_open
+          end
+        end
 
         context "and unconfirmed user" do
           let(:user) { create(:user, confirmed_at: nil) }
@@ -119,50 +163,6 @@ RSpec.describe UsersController, type: :controller do
         it "redirects to the user" do
           put :update, params: { id: user.to_param, user: valid_attributes }
           expect(response).to redirect_to(user)
-        end
-
-        context "communication opt-in" do
-          before do
-            Timecop.freeze(Time.new(2002, 10, 31))
-          end
-
-          after do
-            Timecop.return
-          end
-
-          shared_examples_for 'tracks opt-in time' do |attribute|
-            it "sets #{attribute} to the current time" do
-              put :update, params: { id: user.to_param, user: { attribute => '1' } }
-              expect(user.reload.send "#{attribute}_at").to eq(Time.new(2002, 10, 31))
-            end
-          end
-
-          it_behaves_like 'tracks opt-in time', :opted_in_newsletter
-          it_behaves_like 'tracks opt-in time', :opted_in_announcements
-          it_behaves_like 'tracks opt-in time', :opted_in_marketing_announcements
-          it_behaves_like 'tracks opt-in time', :opted_in_surveys
-          it_behaves_like 'tracks opt-in time', :opted_in_sponsorships
-          it_behaves_like 'tracks opt-in time', :opted_in_applications_open
-
-          context 'opting out' do
-            shared_examples_for 'tracks opt-out' do |attribute|
-              before do
-                user.update_attribute attribute, Time.new(2002, 10, 31)
-              end
-
-              it "sets #{attribute} to nil" do
-                put :update, params: { id: user.to_param, user: { attribute => '0' } }
-                expect(user.reload.send "#{attribute}_at").to eq(nil)
-              end
-            end
-
-            it_behaves_like 'tracks opt-out', :opted_in_newsletter
-            it_behaves_like 'tracks opt-out', :opted_in_announcements
-            it_behaves_like 'tracks opt-out', :opted_in_marketing_announcements
-            it_behaves_like 'tracks opt-out', :opted_in_surveys
-            it_behaves_like 'tracks opt-out', :opted_in_sponsorships
-            it_behaves_like 'tracks opt-out', :opted_in_applications_open
-          end
         end
       end
     end
