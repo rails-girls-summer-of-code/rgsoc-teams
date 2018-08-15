150cad332db6823a17d9fc9b7a7f7ab80ea5df03
Ramon Huidobro <hola@ramonhuidobro.com>
2018-08-15 23:06:15 +0200

    Overhaul spec

--- spec/features/users/opt_in_spec.rb
+++ spec/features/users/opt_in_spec.rb
@@ -3,31 +3,34 @@ require 'rails_helper'
 RSpec.describe 'Email opt in', type: :feature do
   let(:user)      { create(:user) }
 
-  context 'signed in' do
-    before { sign_in user }
-    after { sign_out user }
-
     context 'in the user edit page' do
       before { visit edit_user_path(user) }
 
-      shared_examples 'opting in' do |optin|
-        it 'allows opting in' do
-          find(:css, "##{optin}").set(true)
-          click_on 'Save'
-          visit edit_user_path(user)
-          expect(page.find("##{optin}")).to be_checked
-        end
-      end
+      it 'allows optin in and out of email preferences' do
+        expect(page.find('#user_opted_in_newsletter')).to_not be_checked
+        expect(page.find('#user_opted_in_announcements')).to_not be_checked
+        expect(page.find('#user_opted_in_marketing_announcements')).to_not be_checked
+        expect(page.find('#user_opted_in_surveys')).to_not be_checked
+        expect(page.find('#user_opted_in_sponsorships')).to_not be_checked
+        expect(page.find('#user_opted_in_applications_open')).to_not be_checked
 
-      it_behaves_like 'opting in', :user_opted_in_newsletter
-      it_behaves_like 'opting in', :user_opted_in_announcements
-      it_behaves_like 'opting in', :user_opted_in_marketing_announcements
-      it_behaves_like 'opting in', :user_opted_in_surveys
-      it_behaves_like 'opting in', :user_opted_in_sponsorships
-      it_behaves_like 'opting in', :user_opted_in_applications_open
+        find(:css, "#user_opted_in_applications_open").set(true)
+        find(:css, "#user_opted_in_marketing_announcements").set(true)
+        click_on 'Save'
 
+        visit edit_user_path(user)
+        expect(page.find('#user_opted_in_newsletter')).to_not be_checked
+        expect(page.find('#user_opted_in_announcements')).to_not be_checked
+        expect(page.find('#user_opted_in_marketing_announcements')).to be_checked
+        expect(page.find('#user_opted_in_surveys')).to_not be_checked
+        expect(page.find('#user_opted_in_sponsorships')).to_not be_checked
+        expect(page.find('#user_opted_in_applications_open')).to be_checked
 
-      it 'is opted out of all email preferences by default' do
+        find(:css, "#user_opted_in_applications_open").set(false)
+        find(:css, "#user_opted_in_marketing_announcements").set(false)
+        click_on 'Save'
+
+        visit edit_user_path(user)
         expect(page.find('#user_opted_in_newsletter')).to_not be_checked
         expect(page.find('#user_opted_in_announcements')).to_not be_checked
         expect(page.find('#user_opted_in_marketing_announcements')).to_not be_checked
@@ -35,7 +38,5 @@ RSpec.describe 'Email opt in', type: :feature do
         expect(page.find('#user_opted_in_sponsorships')).to_not be_checked
         expect(page.find('#user_opted_in_applications_open')).to_not be_checked
       end
-
     end
-  end
 end
