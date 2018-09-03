6f7b945b24ea2046e870e4821092b93ff16822fe
Sabrina Robinson <sabrinarob@gmail.com>
2018-09-03 14:57:29 -0600

    Implements forms for street, state, and zip

--- spec/features/users/postal_address_input_spec.rb
+++ spec/features/users/postal_address_input_spec.rb
@@ -11,10 +11,11 @@ RSpec.describe 'Postal address input', type: :feature do
 
       it 'allows address input for shipment' do
         visit edit_user_path(user)
-        fill_in 'street', with: '123 Main St.'
-        fill_in 'state', with: 'CO'
-        fill_in 'zip', with: '12345'
-        click 'Save'
+        # save_and_open_page
+        fill_in 'user_postal_addresses_street', with: '123 Main St.'
+        fill_in 'user_postal_addresses_state', with: 'CO'
+        fill_in 'user_postal_addresses_zip', with: '12345'
+        click_on 'Save'
 
         expect(current_path).to eq(user_path(user))
 
