752ab32c4c14386a4f269b7a730db197ae801667
Margaret Williford <margaretwilliford4@gmail.com>
2018-11-07 13:21:17 -0700

    fixing nested attributes pluralization

--- spec/features/users/postal_address_spec.rb
+++ spec/features/users/postal_address_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe 'Add Postal Address', type: :feature do
       before { visit edit_user_path(user) }
 
       it 'allows creation of postal address if all required address fields are entered' do
-        fill_in 'user_postal_addresses_address_line_1',    with: address.address_line_1
-        fill_in 'user_postal_addresses_address_line_2',    with: address.address_line_2
-        fill_in 'user_postal_addresses_city',              with: address.city
-        fill_in 'user_postal_addresses_state_or_province', with: address.state_or_province
-        fill_in 'user_postal_addresses_postal_code',       with: address.postal_code
-        select address.country, from: 'user_postal_addresses_country'
+        fill_in 'user[postal_address_attributes][address_line_1]',    with: address.address_line_1
+        fill_in 'user[postal_address_attributes][address_line_2]',    with: address.address_line_2
+        fill_in 'user[postal_address_attributes][city]',              with: address.city
+        fill_in 'user[postal_address_attributes][state_or_province]', with: address.state_or_province
+        fill_in 'user[postal_address_attributes][postal_code]',       with: address.postal_code
+        select address.country, from: 'user[postal_address_attributes][country]'
         click_on 'Save'
 
         expect(current_path).to eq user_path(user)
