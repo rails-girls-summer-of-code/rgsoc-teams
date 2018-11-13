b06aa4c2c10f8206379977612a57cb7ee1e3310d
Margaret Williford <margaretwilliford4@gmail.com>
2018-11-13 15:10:03 -0700

    move postal address formatting to partial

--- app/models/postal_address.rb
+++ app/models/postal_address.rb
@@ -3,8 +3,4 @@ class PostalAddress < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :line1, :city, :zip, :country
-
-  def formatted
-    "#{line1.titlecase} #{line2.titlecase} #{city.titlecase}, #{state} #{zip} #{country}"
-  end
 end
