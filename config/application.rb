e6648016fa505899c18cb41c07ee0dee91a62ab0
Max Mulatz <klappradla@posteo.net>
2019-04-06 23:03:55 +0200

    Add explicit autoloading back

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RgsocTeams
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.autoload_paths += %W(#{config.root}/app)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
