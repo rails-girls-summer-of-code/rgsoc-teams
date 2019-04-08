8950a9ebb156d3768616b06f8d2f18302e325340
Max Mulatz <klappradla@posteo.net>
2019-04-08 21:01:10 +0200

    Remove additional appending to autoloading paths

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module RgsocTeams
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.autoload_paths += %W(#{config.root}/app)
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
